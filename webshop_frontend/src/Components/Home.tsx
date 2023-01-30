6019a682f76d413ab94e79019eb2cab32ccf5e29
z-rayc <zilerayser@gmail.com>
2023-01-30 23:27:31 +0100

    feat: :sparkles: create hero image

--- webshop_frontend/src/Components/Home.tsx
+++ webshop_frontend/src/Components/Home.tsx
@@ -1,7 +1,19 @@
+import React from "react";
+
 export default function Home() {
     return (
-        <section className="home">
-            <h1>Home</h1>
-        </section>
+        <React.Fragment>
+            <section className="hero">
+                <div className="hero-inner">
+                    <h1 className="hero-title">ProFlex</h1>
+                    <p className="hero-text">Software for <b>your</b> enterprise</p>
+                    <button className="hero-button">Our offers</button>
+                </div>
+            </section>
+            <section className="home">
+                
+            </section>
+        </React.Fragment>
+        
     );
 }
