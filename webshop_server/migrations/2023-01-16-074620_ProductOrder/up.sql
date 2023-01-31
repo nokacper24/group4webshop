5b5c4d6fafc930c2529058839aad6c37de8e651a
Kacper Lukasz Nowicki <kacperln@ntnu.no>
2023-01-31 20:52:29 +0100

    feat: :card_file_box: replace diesel with sqlx

--- webshop_server/migrations/2023-01-16-074620_ProductOrder/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TABLE ProductOrder (
-    product_order_id SERIAL PRIMARY KEY,
-    product_id INT NOT NULL,
-    Foreign Key (product_id) REFERENCES Product(product_id),
-    order_id INT NOT NULL,
-    Foreign Key (order_id) REFERENCES Orders(order_id),
-    amount INT NOT NULL
-);
