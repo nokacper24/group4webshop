6312564470adb411efcff5beaa4f200f97e5ef07
z-rayc <zilerayser@gmail.com>
2023-01-31 22:54:55 +0100

    Create Partner.tsx

---
+++ webshop_frontend/src/Components/home/Partner.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export type PartnerProps = {
+    partner: {
+        name: string;
+        link: string;
+        imageSize: number[];
+        imageSource: string;
+    }
+}
+
+export const Partner = ({partner}: PartnerProps) => {
+    return (
+        <li>
+            <Link target="_blank" to={partner.link}>
+                <img className="partner-logo" src={partner.imageSource}
+                    width={partner.imageSize[0]}
+                    height={partner.imageSize[1]}
+                    alt={partner.name + " logo"}></img>
+            </Link>
+        </li>
+        
+    )
+}
