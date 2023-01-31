faa69d032ce9139381a28f8a95615cf66f3dc66d
z-rayc <zilerayser@gmail.com>
2023-01-31 22:56:03 +0100

    feat: :sparkles: add "partners" section
    resolves #3

--- webshop_frontend/src/Components/home/Home.tsx
+++ webshop_frontend/src/Components/home/Home.tsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import {Partners} from "./Partners";
 
 class Home extends Component {
+    state = {
+        partners: [
+            {name: 'Acme', link: 'https://unsplash.it/', imageSize: [200, 100], imageSource: 'https://unsplash.it/200/100'},
+            {name: 'Acme2', link: 'https://unsplash.it/', imageSize: [200, 100], imageSource: 'https://unsplash.it/200/100'},
+            {name: 'Acme3', link: 'https://unsplash.it/', imageSize: [200, 100], imageSource: 'https://unsplash.it/200/100'},
+        ]
+    }
     render() {
         return (
             <React.Fragment>
@@ -14,6 +22,10 @@ class Home extends Component {
                 <section className="container">
                     <h2>Our values</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dignissim enim sit amet venenatis urna cursus eget nunc scelerisque. A lacus vestibulum sed arcu non odio euismod lacinia at. Odio ut sem nulla pharetra diam. Ac tortor dignissim convallis aenean et tortor. Massa eget egestas purus viverra accumsan in nisl nisi.</p>
+                </section>
+                <section className="container">
+                    <h2>Our partners</h2>
+                    <Partners partners={this.state.partners}/>
                 </section>
             </React.Fragment>
         );
