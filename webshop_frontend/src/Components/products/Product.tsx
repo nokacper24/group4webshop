e469cf230d63dc6c4a5fb80cddc116f77492e344
Oransj <chrion@stud.ntnu.no>
2023-02-01 22:39:58 +0100

    refactor: :lipstick: assigned classes to product elements

--- webshop_frontend/src/Components/products/Product.tsx
+++ webshop_frontend/src/Components/products/Product.tsx
@@ -10,12 +10,13 @@ export type ProductProps = {
 
 export const Product = ({product}: ProductProps) => {
     return (
-        <li>
+        <li className="product-card">
             <Link to={"#"}>
                 <img height={200}
                     width={300} 
                     src={product.sourceImage}
-                    alt={"Product image of " + product.name}>
+                    alt={"Product image of " + product.name}
+                    className = "card-image">
                 </img>
                 <h2>{product.name}</h2>
                 <p>{product.description}</p>
