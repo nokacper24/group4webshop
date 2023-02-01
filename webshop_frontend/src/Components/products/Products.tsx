2ba8d9da648ea629ae6febf4e12ff4c998f4f965
z-rayc <zilerayser@gmail.com>
2023-02-01 19:55:00 +0100

    fix: :bug: fix typo in import

--- webshop_frontend/src/Components/products/Products.tsx
+++ webshop_frontend/src/Components/products/Products.tsx
@@ -1,12 +1,12 @@
-import { Product } from "./product";
+import { Product } from "./Product";
 
 export default function Products() {
     let state = {
         products: [
-            {name: 'Acme', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ', sourceImage: 'https://unsplash.it/200/100'},
-            {name: 'Acme2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ', sourceImage: 'https://unsplash.it/200/100'},
-            {name: 'Acme3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ', sourceImage: 'https://unsplash.it/200/100'},
-            {name: 'Acme4', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ', sourceImage: 'https://unsplash.it/200/100'},
+            {name: 'Acme', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
+            {name: 'Acme2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
+            {name: 'Acme3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
+            {name: 'Acme4', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
         ]
     }
     return (
@@ -18,8 +18,8 @@ export default function Products() {
                 {
                     state.products.map((product) => (
                         <Product 
-                        key={product.name}
-                        product={product} />
+                            key={product.name}
+                            product={product} />
                     ))
                 }
             </ul>
