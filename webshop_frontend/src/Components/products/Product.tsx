273bf32d109cec5798eb3c8faa06defa58227471
z-rayc <zilerayser@gmail.com>
2023-02-01 19:56:02 +0100

    fix: :bug: fix image- and link closing bracket

--- webshop_frontend/src/Components/products/Product.tsx
+++ webshop_frontend/src/Components/products/Product.tsx
@@ -5,21 +5,21 @@ export type ProductProps = {
         name: string;
         description: string;
         sourceImage: string;
-    
     }
 }
 
 export const Product = ({product}: ProductProps) => {
     return (
         <li>
-            <Link to={"#"}/>¨
-            <img height={200}
-                width={300} 
-                src={product.sourceImage}>
-                alt={"Product image of " + product.name}
-            </img>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
+            <Link to={"#"}>
+                <img height={200}
+                    width={300} 
+                    src={product.sourceImage}
+                    alt={"Product image of " + product.name}>
+                </img>
+                <h2>{product.name}</h2>
+                <p>{product.description}</p>
+            </Link>
         </li>
     )
 }
