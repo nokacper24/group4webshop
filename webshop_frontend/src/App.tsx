bcceb03d82b2c2b47f136bf9cdc72909da3bd1ed
Oransj <98455426+Oransj@users.noreply.github.com>
2023-02-01 19:37:38 +0100

    Update location Products

--- webshop_frontend/src/App.tsx
+++ webshop_frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer';
 import Home from './Components/home/Home';
-import Products from './Components/Products';
+import Products from './Components/products/Products';
 import About from './Components/About';
 import Support from './Components/Support';
 import Profile from './Components/Profile';
