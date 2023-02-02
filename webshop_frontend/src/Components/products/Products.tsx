abfbd41f4617133476c8209fa02e1629e29fd924
Oransj <98455426+Oransj@users.noreply.github.com>
2023-02-02 10:01:36 +0100

    feat: :sparkles: add functionality for loading product props from list

--- webshop_frontend/src/Components/products/Products.tsx
+++ webshop_frontend/src/Components/products/Products.tsx
@@ -1,14 +1,26 @@
-import { Product } from "./Product";
+import { Product, ProductProps} from "./Product";
+
+export type State = {
+    products: ProductProps[];
+}
 
 export default function Products() {
-    let state = {
-        products: [
-            {name: 'Acme', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
-            {name: 'Acme2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
-            {name: 'Acme3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
-            {name: 'Acme4', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
-        ]
+    const website_data = [
+        {name: 'Acme', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
+        {name: 'Acme2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
+        {name: 'Acme3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
+        {name: 'Acme4', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', sourceImage: 'https://unsplash.it/200/100'},
+    ]
+    let state: State = {
+        products: [],
     }
+    website_data.forEach((projectObject) => {
+        let newProduct: ProductProps = {
+            props: projectObject, 
+        }
+        state.products.push(newProduct);
+    });
+
     return (
         <div>
             <section className="container">
@@ -17,8 +29,8 @@ export default function Products() {
                     {
                         state.products.map((product) => (
                             <Product 
-                                key={product.name}
-                                product={product} />
+                                key={product.props.name}
+                                props={product.props} />
                         ))
                     }
                 </ul>
