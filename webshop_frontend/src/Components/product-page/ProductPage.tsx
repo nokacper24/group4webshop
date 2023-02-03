ac9866a47f815bd38c82159cd6610ca8469e10ef
Oransj <chrion@stud.ntnu.no>
2023-02-03 14:59:11 +0100

    feat: :lipstick: add functionality to render the page

--- webshop_frontend/src/Components/product-page/ProductPage.tsx
+++ webshop_frontend/src/Components/product-page/ProductPage.tsx
@@ -1,4 +1,35 @@
+import DescriptionRow, { ProductPageRow, RowItem } from "./DescriptionRow";
+
+export type State = {
+    rows: ProductPageRow[];
+}
+
 export default function ProductPage() {
     }
+    const row_data = [
+        {item1: rowTextItem, item2: rowImageItem, textToLeft: true},
+        {item1: rowImageItem, item2: rowTextItem, textToLeft: false},
+    ]
+    let state: State = {
+        rows: [],
+    }
+    row_data.forEach((row) => {
+        let newRow: ProductPageRow= {
+            props: row, 
+        }
+        state.rows.push(newRow);
+    });
+    let i: number = 0;
     return (
+        <section className="container">
+            <h1 className="hero-title">Profile</h1>
+            {
+                state.rows.map((item) => (
+                    <DescriptionRow
+                        key={i}
+                        props={item.props} />
+                ))
+            }
+        </section>
     );
+}
