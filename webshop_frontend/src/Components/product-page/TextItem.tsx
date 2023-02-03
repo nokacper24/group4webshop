e42dbfaa929b99bec5a9cf05952b3e3a3739434e
Oransj <chrion@stud.ntnu.no>
2023-02-03 14:54:38 +0100

    feat: :lipstick: add item structure

--- webshop_frontend/src/Components/product-page/TextItem.tsx
+++ webshop_frontend/src/Components/product-page/TextItem.tsx
@@ -1,5 +1,20 @@
 import {RowItem} from './DescriptionRow'
 
 export const TextItem = (rowItem: RowItem | undefined) => {
+    let item: JSX.Element;
+    if(rowItem === undefined || rowItem.content === undefined) {
+        item = <div className='empty-item'></div>
+    } else {
+        item = (
+        <div className='text-item-wrapper'>
+            <h2>
+                {rowItem.title}
+            </h2>
+            <p>
+                {rowItem.content}
+            </p>
+        </div>
+        );
+    }
     return item;
 }
