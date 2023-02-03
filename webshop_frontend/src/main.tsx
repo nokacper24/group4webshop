b8b7827bde6c53b980c5e3e4fa736674bd6d2f97
z-rayc <zilerayser@gmail.com>
2023-02-04 00:53:20 +0100

    revert: :fire: remove unused CSS file

--- webshop_frontend/src/main.tsx
+++ webshop_frontend/src/main.tsx
@@ -1,7 +1,6 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />
+);
