7b278519d469ed475c3f05894238e23ecd04a3ec
z-rayc <zilerayser@gmail.com>
2023-02-03 19:48:45 +0100

    style: :art: format code with Prettier

--- webshop_frontend/src/App.tsx
+++ webshop_frontend/src/App.tsx
@@ -1,37 +1,36 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/home/Home';
-import Products from './Components/products/Products';
-import About from './Components/About';
-import Support from './Components/Support';
-import Profile from './Components/Profile';
-import Product_page from './Components/product-page/ProductPage';
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/home/Home";
+import Products from "./Components/products/Products";
+import ProductPage from "./Components/product-page/ProductPage";
+import About from "./Components/About";
+import Support from "./Components/Support";
+import Profile from "./Components/Profile";
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <BrowserRouter>
-            <Header />
+  return (
+    <BrowserRouter>
+      <Header />
 
-            <main id="main">
-                <Routes>
-                    <Route path="/" element={<Home/>} />
-                    <Route path="products" element={<Products/>} />
-                    <Route path="about" element={<About/>} />
-                    <Route path="support" element={<Support/>} />
-                    <Route path="profile" element={<Profile/>} />
-                    <Route path="product-page" element={<Product_page/>} />
-                </Routes>
-            </main>
+      <main id="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="about" element={<About />} />
+          <Route path="support" element={<Support />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="product-page" element={<ProductPage />} />
+        </Routes>
+      </main>
 
-            <Footer />
-        </BrowserRouter>
-
-    )
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
