842543d2a49e7cc6e54a483c62152d313868b813
Oransj <chrion@stud.ntnu.no>
2023-02-03 14:50:46 +0100

    feat: :sparkles: create public RowItem object
    Symbolizes either a paragraph or image in a row

--- webshop_frontend/src/Components/product-page/DescriptionRow.tsx
+++ webshop_frontend/src/Components/product-page/DescriptionRow.tsx
@@ -1,3 +1,8 @@
+export type RowItem = {
+    title: string | undefined,
+    content: string,
+    isTextNotImage: boolean
+}
 export default function DescriptionRow (row: ProductPageRow) {
     return (
     );
