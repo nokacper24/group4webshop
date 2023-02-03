febe9c248b2114cd489c52600751a0a218939ac4
Oransj <chrion@stud.ntnu.no>
2023-02-03 14:54:44 +0100

    feat: :lipstick: add item structure

--- webshop_frontend/src/Components/product-page/ImageItem.tsx
+++ webshop_frontend/src/Components/product-page/ImageItem.tsx
@@ -1,4 +1,15 @@
 import {RowItem} from './DescriptionRow'
+
 export const ImageItem = (rowItem: RowItem | undefined) => {
+    let item: JSX.Element;
+    if(rowItem === undefined ||rowItem.content === undefined) {
+        item = <div className='empty-item'></div>
+    } else {
+        item = (
+        <div className='text-item-wrapper'>
+            <img src={rowItem.content} alt="" />
+        </div>
+        );
+    }
     return item;
 }
