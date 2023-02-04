5b8e889701df4e9ce9ee249304d63c3a4e0db481
z-rayc <zilerayser@gmail.com>
2023-02-04 20:06:04 +0100

    feat: :sparkles: add text and support form

--- webshop_frontend/src/Components/support/Support.tsx
+++ webshop_frontend/src/Components/support/Support.tsx
@@ -4,6 +4,28 @@ import SupportForm from "./SupportForm";
 export default function Support() {
   return (
     <React.Fragment>
+      <section className="container left-aligned">
+        <h1>Support</h1>
+        <p>
+          At Proflex, we understand that our customers are the backbone of our
+          business, which is why we prioritize their needs above all else. Our
+          dedicated support team is here to ensure that you have the best
+          experience possible with our software.
+        </p>
+        <p>
+          Whether you need assistance with installation, troubleshooting, or
+          have any other questions, we are here to help. We offer a variety of
+          support options, including online resources, e-mail, and phone
+          support, to ensure that you get the help you need, when you need it.
+        </p>
+        <p>
+          Additionally, we offer a comprehensive knowledge base that is
+          regularly updated with the latest information, making it easy for you
+          to find answers to your questions. With Proflex, you can rest assured
+          that your satisfaction is our top priority.
+        </p>
+      </section>
+      <SupportForm />
     </React.Fragment>
   );
 }
