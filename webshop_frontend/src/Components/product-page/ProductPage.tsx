277df2bc9d1472f3317389b6d6c22ff6be4119dd
Oransj <chrion@stud.ntnu.no>
2023-02-04 20:10:42 +0100

    style: :art: format code with prettier

--- webshop_frontend/src/Components/product-page/ProductPage.tsx
+++ webshop_frontend/src/Components/product-page/ProductPage.tsx
@@ -4,65 +4,68 @@ import DescriptionRow, { ProductPageRow, RowItem } from "./DescriptionRow";
 let counter = 0;
 
 export type State = {
-    rows: ProductPageRow[];
-}
+  rows: ProductPageRow[];
+};
 
 export default function ProductPage() {
-    let rowTextItem: RowItem = {
-        title: "Lorem",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam vitae tempora tempore dolore, porro reprehenderit suscipit, id sapiente molestias delectus alias saepe a doloribus est enim. Tenetur dolore sapiente eaque.",
-        isTextNotImage: true,
-    }
-    let rowImageItem: RowItem = {
-        title: undefined,
-        content: 'https://unsplash.it/300/200',
-        isTextNotImage: false,
-    }
-    const row_data = [
-        {item1: rowTextItem, item2: rowImageItem, textToLeft: true},
-        {item1: rowImageItem, item2: rowTextItem, textToLeft: false},
-    ]
-    let state: State = {
-        rows: [],
-    }
-    row_data.forEach((row) => {
-        let newRow: ProductPageRow= {
-            props: row, 
-        }
-        state.rows.push(newRow);
-    });
-    return (
-        <React.Fragment>
-            <section className="banner">
-                <div className="banner-inner">
-                    <div className="banner-highlight">
-                        <h1 className="banner-title banner-element hero-title">Lorem ipsum</h1>
-                        <button className="banner-button banner-element hero-button">Buy license now</button>
-                    </div>
-                </div>
-            
-            </section>
-            <section className="product-description">
-                {
-                    state.rows.map((productPageRow) => (
-                        <DescriptionRow
-                            key={assignUniqueKey("row")}
-                            props={productPageRow.props} />
-                            
-                    ))
-                }
-            </section>
-        </React.Fragment>
-    );
+  let rowTextItem: RowItem = {
+    title: "Lorem",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam vitae tempora tempore dolore, porro reprehenderit suscipit, id sapiente molestias delectus alias saepe a doloribus est enim. Tenetur dolore sapiente eaque.",
+    isTextNotImage: true,
+  };
+  let rowImageItem: RowItem = {
+    title: undefined,
+    content: "https://unsplash.it/300/200",
+    isTextNotImage: false,
+  };
+  const row_data = [
+    { item1: rowTextItem, item2: rowImageItem, textToLeft: true },
+    { item1: rowImageItem, item2: rowTextItem, textToLeft: false },
+  ];
+  let state: State = {
+    rows: [],
+  };
+  row_data.forEach((row) => {
+    let newRow: ProductPageRow = {
+      props: row,
+    };
+    state.rows.push(newRow);
+  });
+  return (
+    <React.Fragment>
+      <section className="banner">
+        <div className="banner-inner">
+          <div className="banner-highlight">
+            <h1 className="banner-title banner-element hero-title">
+              Lorem ipsum
+            </h1>
+            <button className="banner-button banner-element hero-button">
+              Buy license now
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="product-description">
+        {state.rows.map((productPageRow) => (
+          <DescriptionRow
+            key={assignUniqueKey("row")}
+            props={productPageRow.props}
+          />
+        ))}
+      </section>
+      <section></section>
+    </React.Fragment>
+  );
 }
 /**
  * When you have multiple items with the same key, this adds an unique ID behind the key
  * and returns it.
- * 
+ *
  * @param base the string you want an unique ID added to
  * @returns the string modified to include the ID
  */
 function assignUniqueKey(base: string) {
-    counter++;
-    return base + counter;
+  counter++;
+  return base + counter;
 }
