755daf19d90bafd11fd2f4afde58940fe6f85e12
z-rayc <zilerayser@gmail.com>
2023-02-04 16:56:20 +0100

    feat: :sparkles: add Contact Us section
    resolves #15

--- webshop_frontend/src/Components/about-us/About.tsx
+++ webshop_frontend/src/Components/about-us/About.tsx
@@ -31,9 +31,33 @@ export default function About() {
           quickly and easily access the resources they need to stay ahead of the
           competition.
         </p>
+        <p>
+          We are committed to providing businesses with the best products and
+          services to help them succeed. Our core values are
+          customer-centricity, innovation, and transparency. We strive to
+          provide excellent customer service and build lasting relationships
+          with our clients. We are always looking for new ways to innovate and
+          improve our products and services. We are committed to being open and
+          honest with our clients about our processes and the services we offer.
+          With Proflex Solutions, you can trust that you're getting the best
+          products and services for your business.
+        </p>
       </section>
       <section className="container left-aligned">
         <h2>Contact us</h2>
+        <p>
+          <b>Phone number:</b> <a href="tel:+00000000">+000 00 000</a>
+        </p>
+        <p>
+          <b>E-mail:</b>{" "}
+          <a href="mailto:contact@proflex.com">contact@proflex.com</a>
+        </p>
+        <p>
+          <b>Address:</b> <br></br>
+          Streetname 04<br></br>
+          0404 Cityname<br></br>
+          Countryname
+        </p>
       </section>
     </React.Fragment>
   );
