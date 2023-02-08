74b866adcfeb2793c43a4d3e467ed2bb4f4a1966
z-rayc <zilerayser@gmail.com>
2023-02-08 14:02:56 +0100

    refactor: :feat: rename from CreateAccount_register.tsx

--- webshop_frontend/src/Components/profile/RegisterEmail.tsx
+++ webshop_frontend/src/Components/profile/RegisterEmail.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 /**
- * Represents the Register component on the Create Account page.
+ * Represents the Register E-mail component on the Create Account page.
  *
  * Allows the user to register their e-mail, and receive a
  * verification link to create their account.
  *
  * @returns The Create Account register component as a JSX element.
  */
-export function CreateAccountRegister() {
+export function RegisterEmail() {
   return (
     <React.Fragment>
       <p>
-        Type in your business e-mail and we will send you a registration e-mail
+        Type in your business e-mail and we will send you a registration link.
+        This is only intended for license managers.
+      </p>
+      <p>
+        Not a license manager? Contact yours to get a direct invitation link.
       </p>
 
       <label htmlFor="register-email">E-mail</label>
