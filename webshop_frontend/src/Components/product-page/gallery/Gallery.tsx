baafe2097cf7cdbdcb5b7d07b8e24a7261fcd063
Oransj <98455426+Oransj@users.noreply.github.com>
2023-02-09 16:04:19 +0100

    refactor: :recycle: refactor slideprops to contain list of slideprops

--- webshop_frontend/src/Components/product-page/gallery/Gallery.tsx
+++ webshop_frontend/src/Components/product-page/gallery/Gallery.tsx
@@ -1,41 +1,31 @@
 import { Component } from "react";
+import { ParagraphSlide } from "./ParagraphSlide";
 import { SlideType } from "./SlideTypes";
 
 export type SlidesProps = {
-  mainContent: string;
-  reviewerProfile: {
-    picturePath: string;
-    name: string;
-    title: string;
-  };
-  slideType: SlideType;
+  slides: {
+    mainContent: string;
+    reviewerProfile: {
+      picturePath: string;
+      name: string;
+      title: string;
+    };
+    slideType: SlideType;
+  }[];
 };
 
-export default class Gallery extends Component<SlidesProps[]> {
-  
-
+export default class Gallery extends Component<SlidesProps> {
   main() {
-    
-    let slide: JSX.Element[];
+    let slides: JSX.Element[];
 
-    this.props.forEach(function (prop) {
-      switch (prop.slideType) {
+    this.props.slides.forEach(function (prop) {
+      switch (
+        prop.slideType //Using switch in case of future expandibiliy, if we ever create more slide types
+      ) {
         case SlideType.PARAGRAPH: {
+          slides.push(<ParagraphSlide slides={prop} />); //adds a ready made slide component to the list.
         }
       }
     });
   }
 }
-
-/* export default function (slideProps: SlideProps[]) {
-  let slide: JSX.Element[];
-
-  slideProps.forEach(function (prop) {
-    switch (prop.slideType) {
-      case SlideType.PARAGRAPH: {
-      }
-    }
-  });
-
-  return <div></div>;
-} */
