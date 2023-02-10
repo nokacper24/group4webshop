8edd48cbe0f0325d9968f7326bba29f2c8b87466
z-rayc <zilerayser@gmail.com>
2023-02-10 20:32:52 +0100

    feat: :sparkles: create a license list table component

---
+++ webshop_frontend/src/Components/profile/managing/LicenseList.tsx
@@ -0,0 +1,61 @@
+import LicenseListRow from "./LicenseListRow";
+
+/**
+ * List of licenses (placeholder).
+ */
+const licenses = [
+  {
+    name: "Planner",
+    activeUsers: 14,
+    total: 15,
+    status: "Active",
+    details: ["01.01.2023", "01.07.2023"],
+  },
+  {
+    name: "Legal",
+    activeUsers: 11,
+    total: 50,
+    status: "Active",
+    details: ["01.01.2023", "01.07.2023"],
+  },
+  {
+    name: "3D Model",
+    activeUsers: 28,
+    total: 40,
+    status: "Active",
+    details: ["01.01.2023", "01.07.2023"],
+  },
+  {
+    name: "Tax",
+    activeUsers: 1,
+    total: 5,
+    status: "Active",
+    details: ["01.01.2023", "01.07.2023"],
+  },
+];
+
+/**
+ * A list of all owned licenses for a user, and their details.
+ *
+ * @returns A License List component as a JSX element.
+ */
+export default function LicenseList() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Product</th>
+          <th>Active</th>
+          <th>Total</th>
+          <th>Status</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {licenses.map((license) => (
+          <LicenseListRow key={license.name} license={license} />
+        ))}
+      </tbody>
+    </table>
+  );
+}
