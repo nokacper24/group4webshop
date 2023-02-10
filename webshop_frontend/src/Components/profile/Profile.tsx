336864ab7556eb786923fe84609c426951cc4c61
z-rayc <zilerayser@gmail.com>
2023-02-10 15:08:16 +0100

    refactor: :sparkles: replace class with function and update docs

--- webshop_frontend/src/Components/profile/Profile.tsx
+++ webshop_frontend/src/Components/profile/Profile.tsx
@@ -1,16 +1,37 @@
-import { Component } from "react";
+import React from "react";
+import LicenseList from "./managing/LicenseList";
 
 /**
  * Represents the Profile page.
  *
  * Contains information about user account and owned licenses.
  */
-export default class Profile extends Component {
-  render() {
-    return (
-      <section className="container">
-        <h1>Profile</h1>
+
+/**
+ * Represents the Profile page.
+ * Contains information about the user acccount and owned licenses.
+ *
+ * @returns The Profile page component as a JSX element.
+ */
+export default function Profile() {
+  return (
+    <React.Fragment>
+      <section className="container left-aligned">
+        <h1>My account</h1>
+        <p>
+          E-mail: user@company.com
+          <button className="default-button small-button">Edit</button>
+          <br></br>
+          Company: Acme
+        </p>
+        <p>
+          <a href="">Reset password</a>
+        </p>
       </section>
-    );
-  }
+      <section className="container left-aligned">
+        <h2>Licenses</h2>
+        <LicenseList />
+      </section>
+    </React.Fragment>
+  );
 }
