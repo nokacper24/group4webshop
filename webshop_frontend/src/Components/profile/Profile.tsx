19e5246123f66c1c9e5dc34bef47966bb86673c1
z-rayc <zilerayser@gmail.com>
2023-02-10 23:24:31 +0100

    feat: :sparkles: show SignIn if not authenticated and MyAccount otherwise

--- webshop_frontend/src/Components/profile/Profile.tsx
+++ webshop_frontend/src/Components/profile/Profile.tsx
@@ -1,37 +1,28 @@
-import React from "react";
-import LicenseList from "./managing/LicenseList";
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import MyAccount from "./MyAccount";
+import SignIn from "./SignIn";
 
-/**
- * Represents the Profile page.
- *
- * Contains information about user account and owned licenses.
- */
-
-/**
- * Represents the Profile page.
- * Contains information about the user acccount and owned licenses.
- *
- * @returns The Profile page component as a JSX element.
- */
 export default function Profile() {
-  return (
-    <React.Fragment>
-      <section className="container left-aligned">
-        <h1>My account</h1>
-        <p>
-          E-mail: user@company.com
-          <button className="default-button small-button">Edit</button>
-          <br></br>
-          Company: Acme
-        </p>
-        <p>
-          <a href="">Reset password</a>
-        </p>
-      </section>
-      <section className="container left-aligned">
-        <h2>Licenses</h2>
-        <LicenseList />
-      </section>
-    </React.Fragment>
-  );
+  const [authenticated, setAuthenticated] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loggedInUser: string | null = localStorage.getItem("authenticated");
+    if (loggedInUser) {
+      setAuthenticated(loggedInUser);
+    }
+  }, []);
+
+  if (!authenticated) {
+    return <SignIn />;
+  } else {
+    return <MyAccount />;
+  }
+
+  /* return (
+    <Routes>
+      <Route path="sign-in" element={<SignIn />}></Route>
+      <Route path="my-account" element={<MyAccount />} />
+    </Routes>
+  ); */
 }
