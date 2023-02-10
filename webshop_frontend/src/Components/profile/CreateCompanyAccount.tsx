77f855f085476bd5370c955b8f9c890eae7f10f5
z-rayc <zilerayser@gmail.com>
2023-02-10 22:48:49 +0100

    refactor: :sparkles: use functional component instead of class

--- webshop_frontend/src/Components/profile/CreateCompanyAccount.tsx
+++ webshop_frontend/src/Components/profile/CreateCompanyAccount.tsx
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import { Routes, Route } from "react-router-dom";
 import { RegisterEmail } from "./RegisterEmail";
 import { EmailVerify } from "./EmailVerify";
@@ -7,29 +6,25 @@ import RegisterUser from "./RegisterUser";
 
 /**
  * Represents the Create Company Account page.
- *
  * Lets the user register their e-mail, tells them to check their
  * e-mail inbox for verification link, and allows them to input
  * company information and user profile password.
+ *
+ * @returns A Create Company Account page.
  */
-export default class CreateCompanyAccount extends Component {
-  render() {
-    return (
-      <div className="center-container">
-        <form className="container form-container">
-          <h1>Register company account</h1>
+export default function CreateCompanyAccount() {
+  return (
+    <div className="center-container">
+      <form className="container form-container">
+        <h1>Register company account</h1>
 
-          <Routes>
-            <Route path="/" element={<RegisterEmail />}></Route>
-            <Route path="verify" element={<EmailVerify />}></Route>
-            <Route
-              path="register-company"
-              element={<RegisterCompany />}
-            ></Route>
-            <Route path="register-user" element={<RegisterUser />} />
-          </Routes>
-        </form>
-      </div>
-    );
-  }
+        <Routes>
+          <Route path="/" element={<RegisterEmail />}></Route>
+          <Route path="verify" element={<EmailVerify />}></Route>
+          <Route path="register-company" element={<RegisterCompany />}></Route>
+          <Route path="register-user" element={<RegisterUser />} />
+        </Routes>
+      </form>
+    </div>
+  );
 }
