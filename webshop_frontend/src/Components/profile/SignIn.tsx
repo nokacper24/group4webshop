923dff7ebf9d3b02eca6267d6c07c311266f545a
z-rayc <zilerayser@gmail.com>
2023-02-10 23:24:57 +0100

    feat: :sparkles: add a placeholder method to authenticate signing in

--- webshop_frontend/src/Components/profile/SignIn.tsx
+++ webshop_frontend/src/Components/profile/SignIn.tsx
@@ -1,47 +1,60 @@
-import { Component } from "react";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 /**
  * Represents the Sign In form for users to access their profile.
+ *
+ * @returns A Sign In component.
  */
-export default class SignIn extends Component {
-  render() {
-    return (
-      <div className="center-container">
-        <form className="container form-container">
-          <h1>Sign in</h1>
-          <p>
-            Not a customer yet?
-            <Link to="/create-account"> Register here!</Link>
-          </p>
+export default function SignIn() {
+  const email = useRef(null);
+  const password = useRef(null);
 
-          <label htmlFor="sign-in-email">E-mail</label>
-          <input
-            id="sign-in-email"
-            name="email"
-            type="text"
-            placeholder="user@company.com"
-            required
-          ></input>
+  const verifySignInInfo = () => {
+    // Placeholder solution. TODO: Implement real solution.
+    if (email.current) {
+      localStorage.setItem("authenticated", email.current.value);
+    }
+  };
 
-          <label htmlFor="sign-in-password">Password</label>
-          <input
-            id="sign-in-password"
-            name="password"
-            type="password"
-            placeholder="password"
-            required
-          ></input>
+  return (
+    <div className="center-container">
+      <form className="container form-container">
+        <h1>Sign in</h1>
+        <p>
+          Not a customer yet?
+          <Link to="/create-account"> Register here!</Link>
+        </p>
 
-          <button
-            className="default-button m-t-1"
-            type="submit"
-            value="Sign in"
-          >
-            Sign in
-          </button>
-        </form>
-      </div>
-    );
-  }
+        <label htmlFor="sign-in-email">E-mail</label>
+        <input
+          ref={email}
+          id="sign-in-email"
+          name="email"
+          type="text"
+          placeholder="user@company.com"
+          required
+        ></input>
+
+        <label htmlFor="sign-in-password">Password</label>
+        <input
+          ref={password}
+          id="sign-in-password"
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+        ></input>
+
+        <button
+          onClick={verifySignInInfo}
+          className="default-button m-t-1"
+          type="submit"
+          value="Sign in"
+        >
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
 }
