07ed869c2cec32555b7e738ce55eacf69f11132f
z-rayc <zilerayser@gmail.com>
2023-02-10 22:28:43 +0100

    refactor: :sparkles: use functional component instead of class

--- webshop_frontend/src/Components/product-page/PurchaseLicense.tsx
+++ webshop_frontend/src/Components/product-page/PurchaseLicense.tsx
@@ -1,81 +1,78 @@
-import React, { Component } from "react";
+import React, { useState, useRef } from "react";
 import LicensePrices from "./LicensePrices";
 
 /**
  * Represents a Purchase License page.
- *
  * Contains a short product description, the plan options, and
- * a checkbox that the user accepts the terms and conditions.
+ * a checkbox for accepting terms and conditions.
+ *
+ * @returns The Purchase License page as a JSX element.
  */
-export default class PurchaseLicense extends Component {
-  state = {
-    productName: "Placeholder Name",
-    productDescription:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, beatae cum. Sequi id sint quos beatae! Similique, molestias enim explicabo obcaecati a iste voluptates repellat? Earum a possimus quo itaque.",
-    pricePerLicense: 15,
-    totalPrice: 0,
-  };
+export default function PurchaseLicense() {
+  const name = "Placeholder Name";
+  const description =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, sequi.";
+  const pricePerLicense = 15;
+  const [totalPrice, setTotalPrice] = useState<number>(0);
 
   /**
    * Update the total price in the object's state.
    *
    * @param event The user event.
    */
-  updatePrice = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    this.setState({ totalPrice: event.target.value });
+  const updatePrice = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setTotalPrice(parseInt(event.target.value));
   };
 
-  render() {
-    return (
-      <section className="container">
-        <h1>Purchase License &rarr; {this.state.productName}</h1>
-        <form className="left-aligned">
-          <p>{this.state.productDescription}</p>
+  return (
+    <section className="container">
+      <h1>Purchase License &rarr; {name}</h1>
+      <form className="left-aligned">
+        <p>{description}</p>
 
-          <LicensePrices
-            price={this.state.pricePerLicense}
-            updatePrice={this.updatePrice}
-          />
+        <LicensePrices
+          price={pricePerLicense}
+          updatePrice={(event) => updatePrice(event)}
+        />
 
-          <p className="total-price">TOTAL: {this.state.totalPrice}</p>
+        <p className="total-price">TOTAL: {totalPrice}</p>
 
-          <button
-            type="submit"
-            className="default-button submit-button"
-            onClick={(event) => this.validateForm(event)}
-          >
-            Buy
-          </button>
-          <div className="checkbox-input">
-            <input id="accept-terms" type="checkbox" required />
-            <label htmlFor="accept-terms">
-              I have read and agree to the <a href="#!">terms of service</a>.
-            </label>
-          </div>
-          <p className="form-alert"></p>
-        </form>
-      </section>
-    );
-  }
+        <button
+          type="submit"
+          className="default-button submit-button"
+          onClick={(event) => validateForm(event)}
+        >
+          Buy
+        </button>
+        <div className="checkbox-input">
+          <input id="accept-terms" type="checkbox" required />
+          <label htmlFor="accept-terms">
+            I have read and agree to the <a href="#!">terms of service</a>.
+          </label>
+        </div>
+        <p className="form-alert"></p>
+      </form>
+    </section>
+  );
+}
 
-  /**
-   * Confirm that all the form's input is valid.
-   *
-   * If the user has not selected an option for the plan,
-   * inform the user that their input is needed.
-   *
-   * @param event Mouse Event on button
-   */
-  validateForm(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    const prices: HTMLSelectElement | null = document.querySelector("#prices");
-    const formAlert: HTMLElement | null = document.querySelector(".form-alert");
+/**
+ * Confirm that all the form's input is valid.
+ *
+ * If the user has not selected an option for the plan,
+ * inform the user that their input is needed.
+ *
+ * @param event Mouse Event on button
+ */
+function validateForm(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+  const prices: HTMLSelectElement | null = document.querySelector("#prices");
+  const formAlert: HTMLElement | null = document.querySelector(".form-alert");
 
-    if (prices?.selectedIndex == 0) {
-      event.preventDefault();
+  if (prices?.selectedIndex == 0) {
+    event.preventDefault();
 
-      if (formAlert != null) {
-        formAlert.innerHTML = "Please select a plan";
-      }
+    if (formAlert != null) {
+      formAlert.innerHTML = "Please select a plan";
     }
   }
 }
