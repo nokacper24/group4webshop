cac405f40d8a826c74f73a03808865e9cd8fbae1
z-rayc <zilerayser@gmail.com>
2023-02-11 20:44:18 +0100

    refactor: :sparkles: create separate function for user authentication

--- webshop_frontend/src/Components/profile/Profile.tsx
+++ webshop_frontend/src/Components/profile/Profile.tsx
@@ -2,13 +2,19 @@ import { useState, useEffect } from "react";
 import MyAccount from "./MyAccount";
 import SignIn from "./SignIn";
 
+/**
+ * The user Profile page.
+ * If the user is already signed in, show the My Account page.
+ * If the user is not signed in, show the Sign In page.
+ *
+ * @returns The Profile page component.
+ */
 export default function Profile() {
-  const [authenticated, setAuthenticated] = useState<string | null>(null);
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
 
   useEffect(() => {
-    const loggedInUser: string | null = localStorage.getItem("authenticated");
-    if (loggedInUser) {
-      setAuthenticated(loggedInUser);
+    if (authenticatedUser()) {
+      setAuthenticated(true);
     }
   }, []);
 
@@ -17,4 +23,15 @@ export default function Profile() {
   } else {
     return <MyAccount />;
   }
+}
+
+/**
+ * Check if the user is authenticated.
+ *
+ * @returns true the user is authenticated, false if not.
+ */
+function authenticatedUser(): boolean {
+  // Authenticate token
+  // Placeholder authentication
+  return localStorage.getItem("authenticated") != null;
 }
