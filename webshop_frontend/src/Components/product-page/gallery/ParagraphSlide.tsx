2da2b284307a21a2c045954cb15005b3a7b89118
Oransj <chrion@stud.ntnu.no>
2023-02-12 16:08:35 +0100

    feat: :lipstick: add basic layout of html in slide

--- webshop_frontend/src/Components/product-page/gallery/ParagraphSlide.tsx
+++ webshop_frontend/src/Components/product-page/gallery/ParagraphSlide.tsx
@@ -1,9 +1,25 @@
 import { SlidesProps } from "./Gallery";
 
 export type ParagraphSlideProps = {
-  
+  paragraph: String,
+  reviewerProfile: {
+    picturePath: string;
+    name: string;
+    title: string;
+  };
 }
 
-export const ParagraphSlide = (props: SlidesProps) => {
-  return <div></div>;
+export const ParagraphSlide = (props: ParagraphSlideProps) => {
+  return <div className="slide">
+    <p>
+      {
+        props.paragraph
+      }
+    </p>
+    <div className="reviewer">
+      <img src={props.reviewerProfile.picturePath} alt="" />
+      <p>{props.reviewerProfile.name}</p>
+      <p>{props.reviewerProfile.title}</p>
+    </div>
+  </div>
 };
