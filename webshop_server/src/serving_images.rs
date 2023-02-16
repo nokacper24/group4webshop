4005837094a2dc5433c03fbd838a4b3de82cb0c3
Kacper Lukasz Nowicki <kacperln@ntnu.no>
2023-02-16 09:44:26 +0100

    fix: :goal_net: proper error handling for image serving

--- webshop_server/src/serving_images.rs
+++ webshop_server/src/serving_images.rs
@@ -1,15 +1,15 @@
 use actix_files::NamedFile;
-use actix_web::http::header::{ContentDisposition, DispositionType};
-use actix_web::{get, App, Error, HttpRequest, Responder, HttpResponse};
+use actix_web::{get, HttpRequest, HttpResponse, Responder};
 
-#[get("/images/{filename:.*}")]
-async fn get_image(
-    req: HttpRequest,
-) -> impl Responder {
-    let path: std::path::PathBuf = req.match_info().query("filename").parse().unwrap();
+#[get("/resources/images/{filename:.*}")]
+async fn get_image(req: HttpRequest) -> impl Responder {
+    let path: std::path::PathBuf = match req.match_info().query("filename").parse() {
+        Ok(path) => path,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
     let path = std::path::Path::new("resources/images/").join(path);
-    let file = NamedFile::open(path).unwrap();
-    let response = file.into_response(&req);
-
-    response
+    match NamedFile::open(path) {
+        Ok(file) => file.into_response(&req),
+        Err(_) => HttpResponse::NotFound().finish(),
+    }
 }
