475ad6607812a55cc24306c0fa8e968d28af89c4
Oransj <98455426+Oransj@users.noreply.github.com>
2023-02-18 10:55:28 +0100

    refactor: :art: reformat with prettier

--- webshop_frontend/src/Components/product-page/gallery/ParagraphSlide.tsx
+++ webshop_frontend/src/Components/product-page/gallery/ParagraphSlide.tsx
@@ -1,27 +1,30 @@
-import { SlidesProps } from "./Gallery";
+import { GalleryProps } from "./Gallery";
 
 export type ParagraphSlideProps = {
-  paragraph: String,
+  id: string;
+  paragraph: string;
   reviewerProfile: {
     picturePath: string;
     name: string;
     title: string;
   };
-}
+};
 
 export const ParagraphSlide = (props: ParagraphSlideProps) => {
-  return <div className="slide">
-    <p>
-      {
-        props.paragraph
-      }
-    </p>
-    <div className="reviewer-profile">
-      <img src={props.reviewerProfile.picturePath} alt="" className="reviewer-picture"/>
-      <div className="reviewer-details">
-        <p>{props.reviewerProfile.name}</p>
-        <p>{props.reviewerProfile.title}</p>
+  return (
+    <div className="slide" id={props.id}>
+      <p>{props.paragraph}</p>
+      <div className="reviewer-profile">
+        <img
+          src={props.reviewerProfile.picturePath}
+          alt=""
+          className="reviewer-picture"
+        />
+        <div className="reviewer-details">
+          <p>{props.reviewerProfile.name}</p>
+          <p>{props.reviewerProfile.title}</p>
+        </div>
       </div>
     </div>
-  </div>
+  );
 };
