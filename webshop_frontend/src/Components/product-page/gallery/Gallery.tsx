80505c55396ecb4af08346f9548665295e3c603f
Oransj <chrion@stud.ntnu.no>
2023-02-18 14:46:56 +0100

    fix: :bug: fix gallery not rotating correctly
    Resolve #10

--- webshop_frontend/src/Components/product-page/gallery/Gallery.tsx
+++ webshop_frontend/src/Components/product-page/gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import { Component, useRef, useState } from "react";
+import { Component, useEffect, useRef, useState } from "react";
 import { ParagraphSlide } from "./ParagraphSlide";
 import { SlideType } from "./SlideTypes";
 
@@ -18,13 +18,34 @@ export type GalleryProps = {
 
 export default function Gallery(props: GalleryProps) {
   let container = useRef<HTMLDivElement>(null);
-  let index: number = 0;
+  let prevButton = useRef<HTMLAnchorElement>(null);
+  let nextButton = useRef<HTMLAnchorElement>(null);
+  const [index, setIndex] = useState<number>(0);
+
+  useEffect(() => {
+    if (props.slides.length > 1) {
+      changeSlide(0);
+    } else {
+      if (prevButton.current && nextButton.current) {
+        prevButton.current.style.display = "none";
+        nextButton.current.style.display = "none";
+      }
+    }
+  }, []);
 
   const [prevSlide, setPrevSlide] = useState<string>("");
   const [nextSlide, setNextSlide] = useState<string>("");
 
   const changeSlide = (amount: number) => {
-    index += amount;
+    let currentIndex = index + amount;
+
+    if (currentIndex >= props.slides.length) {
+      currentIndex = 0;
+    } else if (currentIndex < 0) {
+      currentIndex = props.slides.length - 1;
+    }
+    setIndex(currentIndex);
+
     let prev = index - 1;
     if (prev < 0) {
       prev = props.slides.length - 1;
@@ -46,6 +67,7 @@ export default function Gallery(props: GalleryProps) {
         className="icon-button slide-button"
         href={"#" + prevSlide}
         onClick={() => changeSlide(-1)}
+        ref={prevButton}
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <title>Previous slide</title>
@@ -85,6 +107,7 @@ export default function Gallery(props: GalleryProps) {
         className="icon-button slide-button"
         href={"#" + nextSlide}
         onClick={() => changeSlide(1)}
+        ref={nextButton}
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <title>Next slide</title>
