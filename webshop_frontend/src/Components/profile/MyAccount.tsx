4c4b17b03121b5818adf0aee8a6e69bcb02c7fd2
z-rayc <zilerayser@gmail.com>
2023-02-20 14:05:08 +0100

    feat: :sparkles: fetch user data from API

--- webshop_frontend/src/Components/profile/MyAccount.tsx
+++ webshop_frontend/src/Components/profile/MyAccount.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import LicenseList from "./managing/LicenseList";
+
+let baseUrl = import.meta.env.VITE_URL + ":" + import.meta.env.VITE_PORT;
+
+type UserProps = {
+  email: string;
+  companyId: string;
+};
 
 /**
  * Represents the My Account page.
@@ -8,14 +16,32 @@ import LicenseList from "./managing/LicenseList";
  * @returns The My Account page component.
  */
 export default function MyAccount() {
+  const { id } = useParams();
+  const [user, setUser] = useState<UserProps>();
+
+  const fetchUser = async () => {
+    const response = await fetch(`${baseUrl}/api/users/${id}`);
+    const data = await response.json();
+    const user = {
+      email: data.email,
+      companyId: data.company_id,
+    };
+    setUser(user);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <React.Fragment>
       <section className="container left-aligned">
         <h1>My account</h1>
         <div className="user-details">
           <p>
-            E-mail: user@company.com <br></br>
-            Company: Acme<br></br>
+            E-mail: {user?.email} <br></br>
+            Company: {user?.companyId}
+            <br></br>
             <a href="">Reset password</a>
           </p>
           <button className="default-button small-button">Edit</button>
