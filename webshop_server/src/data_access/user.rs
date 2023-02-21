a2923d03e83bed69755f47fd5f5f4ce6c22de007
z-rayc <zilerayser@gmail.com>
2023-02-21 14:01:54 +0100

    feat: :sparkles: create function get users by company

--- webshop_server/src/data_access/user.rs
+++ webshop_server/src/data_access/user.rs
@@ -37,10 +37,27 @@ pub async fn get_all_users(pool: &Pool<Postgres>) -> Result<Vec<User>, sqlx::Err
 }
 
 pub async fn get_user_by_id(pool: &Pool<Postgres>, user_id: i32) -> Result<User, sqlx::Error> {
-    let product = query_as!(User, r#"SELECT user_id, email, pass_hash, company_id, role as "role: _" FROM app_user WHERE user_id = $1"#, user_id)
+    let user = query_as!(User, r#"SELECT user_id, email, pass_hash, company_id, role as "role: _" FROM app_user WHERE user_id = $1"#, user_id)
         .fetch_one(pool)
         .await?;
-    Ok(product)
+    Ok(user)
+}
+
+/// Returns all users for a company
+pub async fn get_users_by_company(
+    pool: &Pool<Postgres>,
+    company_id: &i32,
+) -> Result<Vec<User>, sqlx::Error> {
+    let users = query_as!(
+        User,
+        r#"SELECT user_id, email, pass_hash, company_id, role as "role: _" 
+        FROM app_user 
+        WHERE company_id = $1"#,
+        company_id
+    )
+    .fetch_all(pool)
+    .await?;
+    Ok(users)
 }
 
 pub async fn get_role_by_id(pool: &Pool<Postgres>, user_id: i32) -> Result<Role, sqlx::Error> {
