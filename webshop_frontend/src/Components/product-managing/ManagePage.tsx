48cbe8d61149d079214aac548c2283880b51be16
Oransj <98455426+Oransj@users.noreply.github.com>
2023-02-22 13:16:30 +0100

    Implement basic HTML layout
    closes #42

--- webshop_frontend/src/Components/product-managing/ManagePage.tsx
+++ webshop_frontend/src/Components/product-managing/ManagePage.tsx
@@ -1,4 +1,45 @@
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
-export function ManagePage() {
-  return <div>Hello</div>;
+export default function ManagePage() {
+  return (
+    <>
+      <h2>Manage product</h2>
+      <section>
+        <form>
+          <label htmlFor="product-name">Product name:</label>
+          <input type="text" id="product-name" name="product-name" />
+          <label htmlFor="product-price">Product price:</label>
+          <div>
+            <input type="number" id="product-price" name="product-price" />
+            <p>kr</p>
+          </div>
+          <label htmlFor="product-image">Upload header image</label>
+          <input
+            type="file"
+            id="product-image"
+            name="product-image"
+            accept="image/png, image/jpeg, image/webp"
+          />
+          <label htmlFor="product-description">Description:</label>
+          <textarea
+            id="product-description"
+            name="product-description"
+            rows={10}
+            cols={50}
+          />
+        </form>
+      </section>
+      <section>
+        <DndProvider backend={HTML5Backend}></DndProvider>
+      </section>
+      <section>
+        <iframe src=""></iframe>
+      </section>
+      <section>
+        <button>Delete product permanently</button>
+        <button>Save</button>
+      </section>
+    </>
+  );
 }
