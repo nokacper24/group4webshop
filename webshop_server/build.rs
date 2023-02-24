cc97aa906112969a75a3651a56ce9a4b25c6c7df
Kacper Lukasz Nowicki <kacperln@ntnu.no>
2023-02-24 12:15:58 +0100

    feat: :sparkles: add automatic building of react
    react is built automatically on release buiild or when BUILD_REACT env var is set to true
    
    related to #41

--- webshop_server/build.rs
+++ webshop_server/build.rs
@@ -1,5 +1,58 @@
 use static_files::resource_dir;
+use std::process::Command;
+
+const FRONTEND_DIR: &str = "../webshop_frontend";
+const REACT_OUT_DIR: &str = "../webshop_frontend/dist";
+const DUMMY_OUT_DIR: &str = "/target/debug/build/webshop_server-dummy-hmtl";
 
 fn main() -> std::io::Result<()> {
-    resource_dir("./static").build()
+    println!("cargo:rerun-if-changed={}", FRONTEND_DIR);
+    println!("cargo:rerun-if-changed=.env");
+    println!("cargo:rerun-if-env-changed=BUILD_REACT");
+
+    dotenvy::dotenv().ok();
+    let build_react = std::env::var("BUILD_REACT").unwrap_or("false".to_string());
+
+    // If BUILD_REACT=true, or if build in release mode, build the react app
+    if build_react.eq_ignore_ascii_case("true") || !cfg!(debug_assertions) {
+        let (program, c_option) = match cfg!(target_os = "windows") {
+            true => ("cmd", "/C"), // windows
+            false => ("sh", "-c"), // unix
+        };
+        let status = Command::new(program)
+            .args(&[c_option, "npm run build"])
+            .current_dir(FRONTEND_DIR)
+            .status()
+            .expect("Failed to run npm");
+        assert!(status.success());
+        resource_dir(REACT_OUT_DIR).build()
+    } else {
+        std::fs::create_dir_all(DUMMY_OUT_DIR).expect("Failed to create dummy dir");
+        std::fs::write(DUMMY_OUT_DIR.to_string() + "/index.html", dummy_hmtl())
+            .expect("Failed to create dummy file");
+        resource_dir(DUMMY_OUT_DIR).build()
+    }
+}
+
+fn dummy_hmtl() -> String {
+    String::from(
+        r#"
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Webshop</title>
+        </head>
+        <body>
+            <h1>Dummy hmtl</h1>
+            <p>Server built without React. In order to build react either:</p>
+            <ul>
+                <li>Set <emp>BUILD_REACT</emp> environment variable to <emp>true</emp></li>
+                <li>Set <emp>BUILD_REACT=true</emp> in <emp>.env</emp></li>
+                <li>Run <emp>cargo build --release</emp></li>
+        </body>
+        </html>
+        "#,
+    )
 }
