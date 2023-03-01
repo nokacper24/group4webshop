e0c1d984469a6c6ae6212660bcc38365dc00649d
Jakob Holkestad Molnes <jakob@dirik.no>
2023-03-01 12:04:49 +0100

    add working logout button

--- webshop_server/src/routes/private/auth.rs
+++ webshop_server/src/routes/private/auth.rs
@@ -1,8 +1,8 @@
-use actix_web::{post, Responder, web, HttpResponse};
+use actix_web::{post, Responder, web::{self, ReqData}, HttpResponse, cookie::time::Duration};
 use serde::{Deserialize, Serialize};
 use sqlx::{Pool, Postgres};
 
-use crate::data_access::{user::get_user_by_username, auth::create_cookie};
+use crate::{data_access::{user::get_user_by_username, auth::{create_cookie, delete_cookie}}, middlewares::auth::Token};
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
 
@@ -12,6 +12,21 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
 
 
 #[post("/logout")]
-async fn logout( pool: web::Data<Pool<Postgres>>) -> impl Responder {
-    "Hello world!"
+async fn logout( pool: web::Data<Pool<Postgres>>, req: ReqData<Token>) -> impl Responder {
+    let token = req.into_inner();
+    // delete cookie from db
+    let token = token.token;
+    let cookie = delete_cookie(&pool, &token).await;
+    match cookie {
+        Ok(_) => {
+            HttpResponse::Ok().cookie(
+                actix_web::cookie::Cookie::build("Bearer", "")
+                    .path("/")
+                    .max_age(Duration::seconds(0))
+                    .finish()
+            ).finish()
+        },
+        Err(_) => HttpResponse::InternalServerError().finish()
+    }
 }
+            
