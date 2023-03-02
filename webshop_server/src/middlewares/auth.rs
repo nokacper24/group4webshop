0b40a9646493a18d37821ff738169bd3b8b99f70
Jakob Holkestad Molnes <jakob@dirik.no>
2023-03-02 23:32:17 +0100

    feat: add getting the user from req token
    also adding comments

--- webshop_server/src/middlewares/auth.rs
+++ webshop_server/src/middlewares/auth.rs
@@ -26,6 +26,7 @@ pub async fn get_cookie(cookie: &str, pool: &Pool<Postgres>) -> Result<auth::Coo
 
 use actix_web_httpauth::extractors::bearer::BearerAuth;
 
+/// Middleware to check if the token is valid and if the user is logged in
 pub async fn validator(
     req: ServiceRequest,
     credentials: BearerAuth,
@@ -50,6 +51,60 @@ pub struct Token {
     pub token: String,
 }
 
+/// Extracts the token from the request and returns the user
+/// # Example
+/// ```
+/// use webshop_server::middlewares::auth;
+///
+/// #[get("/")]
+/// async fn index(req_token: Option<ReqData<auth::Token>>, pool: &Pool<Postgres>) -> Result<HttpResponse, String> {
+///    let user = auth::get_user_from_token(req_token, pool).await;
+///   match user {
+///       Ok(user) => Ok(HttpResponse::Ok().json(user)),
+///      Err(e) => Err(e),
+///  }
+/// }
+/// ```
+/// # Errors
+/// Returns an error if the token is invalid or the user is not found
+pub async fn get_user_from_token(
+    req_token: Option<ReqData<Token>>,
+    pool: &Pool<Postgres>,
+) -> Result<data_access::user::User, String> {
+    match req_token {
+        Some(token) => {
+            let token = token.into_inner();
+            let token = token.token;
+            let cookie = get_cookie(&token, &pool).await;
+            match cookie {
+                Ok(cookie) => {
+                    let user = data_access::user::get_user_by_id(&pool, cookie.user_id).await;
+                    match user {
+                        Ok(user) => Ok(user),
+                        Err(e) => Err(e.to_string()),
+                    }
+                }
+                Err(_) => Err("Unauthorized".to_string()),
+            }
+        }
+        _ => Err("Unauthorized".to_string()),
+    }
+}
+
+/// Extracts the token from the request and returns the role
+///  shorthand for getting role from a logged in user
+/// # Example
+/// ```
+/// use webshop_server::middlewares::auth;
+/// 
+/// #[get("/")]
+/// async fn index(req_token: Option<ReqData<auth::Token>>, pool: &Pool<Postgres>) -> Result<HttpResponse, String> {
+///   let role = auth::check_role(req_token, pool).await;
+///  match role {
+///     Ok(role) => Ok(HttpResponse::Ok().json(role)),
+///    Err(e) => Err(e),
+///     }
+/// }
 pub async fn check_role(
     req_token: Option<ReqData<Token>>,
     pool: &Pool<Postgres>,
