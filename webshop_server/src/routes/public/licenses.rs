fea57b0cbc0a9004c7c65ec9e0a79b964b31100a
z-rayc <zilerayser@gmail.com>
2023-03-02 13:28:02 +0100

    feat: :sparkles: add functions to get vital info and add license

--- webshop_server/src/routes/public/licenses.rs
+++ webshop_server/src/routes/public/licenses.rs
@@ -1,13 +1,15 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, post, web, HttpResponse, Responder};
 use sqlx::{Pool, Postgres};
 use utoipa::OpenApi;
 
-use crate::data_access::license::{self, License};
+use crate::data_access::license::{self, License, PartialLicense};
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(licenses);
+    cfg.service(licenses_vital);
     cfg.service(license_by_id);
     cfg.service(licenses_by_company);
+    cfg.service(add_license);
 }
 
 #[derive(OpenApi)]
@@ -47,6 +49,23 @@ pub async fn licenses(pool: web::Data<Pool<Postgres>>) -> impl Responder {
     // Parse to JSON
     if let Ok(licenses) = licenses {
         return HttpResponse::Ok().json(licenses);
+    }
+
+    HttpResponse::InternalServerError().json("Internal Server Error")
+}
+
+#[get("/licenses_vital")]
+pub async fn licenses_vital(pool: web::Data<Pool<Postgres>>) -> impl Responder {
+    let other_licenses = license::get_licenses_vital_info(&pool).await;
+
+    // Error check
+    if other_licenses.is_err() {
+        return HttpResponse::InternalServerError().json("Internal Server Error");
+    }
+
+    // Parse to JSON
+    if let Ok(other_licenses) = other_licenses {
+        return HttpResponse::Ok().json(other_licenses);
     }
 
     HttpResponse::InternalServerError().json("Internal Server Error")
@@ -125,4 +144,25 @@ async fn licenses_by_company(
     }
 
     HttpResponse::InternalServerError().json("Internal Server Error")
+}
+
+/// Add a license.
+#[utoipa::path (
+    context_path = "/api",
+    post,
+    responses(
+        (status = 201, description = "License has been created", body = License),
+        (status = 500, description = "Internal Server Error"),
+        ),
+    )
+]
+#[post("/licenses")]
+async fn add_license(
+    pool: web::Data<Pool<Postgres>>,
+    license: web::Json<PartialLicense>,
+) -> impl Responder {
+    match license::add_license(&pool, &license).await {
+        Ok(_) => HttpResponse::Created().json(license),
+        Err(_) => HttpResponse::InternalServerError().json("Internal Server Error"),
+    }
 }
