1762f962731f7348b833d7624e21c38122238e0d
Oransj <chrion@stud.ntnu.no>
2023-03-06 20:31:03 +0100

    Add remove button

--- webshop_frontend/src/Components/product-managing/Accordion/AccordionRow.tsx
+++ webshop_frontend/src/Components/product-managing/Accordion/AccordionRow.tsx
@@ -7,6 +7,9 @@ export function AccordionRow() {
                 <input type="checkbox" />
                 <span className="slider"></span>
             </label>
+            <button className="accordion-remove-button">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><title>Remove</title><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M400 256H112"/></svg>
+            </button>
         </div>
     );
 }
