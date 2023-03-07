ec2307e9c68b865561794cbe55b9d018d2dac9a8
Kacper Lukasz Nowicki <kacperln@ntnu.no>
2023-03-07 12:17:14 +0100

    refactor: :recycle: make multipart more generic
    old one is set to deprecated

--- webshop_server/src/routes/private/products_protected/descriptions_protected/description_utils.rs
+++ webshop_server/src/routes/private/products_protected/descriptions_protected/description_utils.rs
@@ -1,7 +1,7 @@
 use actix_multipart::Multipart;
 use futures::StreamExt;
 use image::{io::Reader as ImageReader, DynamicImage, ImageError, ImageFormat, ImageResult};
-use std::io::Cursor;
+use std::{collections::HashMap, io::Cursor};
 
 /// Extracts the image component from a multipart request.
 ///
@@ -19,6 +19,7 @@ use std::io::Cursor;
 /// # Errors
 ///
 /// Returns an `ImageExtractorError` containing the error that occurred.
+#[deprecated]
 pub async fn extract_image_component_from_multipart(
     mut payload: Multipart,
 ) -> Result<(String, Vec<u8>, String), ImageExtractorError> {
@@ -82,6 +83,106 @@ pub enum ImageExtractorError {
     MissingData,
     UnexpectedField(String),
     FileTooLarge,
+}
+
+/// Extracts image and text fields from a multipart request.
+///
+/// # Arguments
+///
+/// * `payload` - The multipart payload to extract the image component from.
+/// * `text_fields` - A vector containing the names of the text fields wanted.
+///
+/// # Returns
+///
+/// A tuple containing the image buffer and the file name, and a vector containing the text fields.
+/// * `image_buffer` - Vector containing the image data as bytes.
+/// * `file_name` - The file name of the image.
+/// * `text_fields` - A vector containing the text fields wanted.
+///
+/// # Errors
+///
+/// Returns an `ImageExtractorError` containing the error that occurred.
+pub async fn extract_image_and_texts_from_multipart(
+    mut payload: Multipart,
+    text_fields: Vec<&str>,
+) -> Result<(Vec<u8>, String, HashMap<String, String>), ImageExtractorError> {
+    let mut file_name = String::new();
+    let mut image_buffer = Vec::new();
+    // map of field name to field value, both strings
+    let mut fields_found: HashMap<String, String> = HashMap::new();
+
+    while let Some(mut item) = payload.next().await {
+        let field = match item {
+            Ok(ref mut field) => field,
+            Err(e) => return Err(ImageExtractorError::MultipartError(e)),
+        };
+        let name = match field.content_disposition().get_name() {
+            Some(name) => name,
+            None => return Err(ImageExtractorError::MissingField("name".to_string())),
+        };
+
+        if name == "image" {
+            extract_image_from_field(field, &mut image_buffer, &mut file_name).await?;
+        } else if text_fields.iter().any(|field| field == &name) {
+            let field_name = String::from(name);
+            let mut text_field = String::new();
+            extract_text_field(field, &mut text_field).await?;
+            fields_found.insert(field_name, text_field);
+        } else {
+            return Err(ImageExtractorError::UnexpectedField(name.to_string()));
+        }
+    }
+    if file_name.trim().is_empty() || image_buffer.is_empty() {
+        return Err(ImageExtractorError::MissingData);
+    }
+
+    if fields_found.len() != text_fields.len() {
+        return Err(ImageExtractorError::MissingData);
+    }
+
+    Ok((image_buffer, file_name, fields_found))
+}
+
+async fn extract_text_field(
+    field: &mut actix_multipart::Field,
+    text_field: &mut String,
+) -> Result<(), ImageExtractorError> {
+    while let Some(chunk) = field.next().await {
+        let data = match chunk {
+            Ok(data) => data,
+            Err(e) => return Err(ImageExtractorError::MultipartError(e)),
+        };
+        let string = match std::str::from_utf8(&data) {
+            Ok(s) => s,
+            Err(e) => return Err(ImageExtractorError::Utf8Error(e)),
+        };
+        text_field.push_str(string);
+    }
+    Ok(())
+}
+
+async fn extract_image_from_field(
+    field: &mut actix_multipart::Field,
+    image_buffer: &mut Vec<u8>,
+    file_name: &mut String,
+) -> Result<(), ImageExtractorError> {
+    file_name.push_str(match field.content_disposition().get_filename() {
+        Some(name) => name,
+        None => return Err(ImageExtractorError::MissingField("filename".to_string())),
+    });
+
+    while let Some(chunk) = field.next().await {
+        let data = match chunk {
+            Ok(data) => data,
+            Err(e) => return Err(ImageExtractorError::MultipartError(e)),
+        };
+        if image_buffer.len() + data.len() > super::MAX_IMAGE_SIZE {
+            return Err(ImageExtractorError::FileTooLarge);
+        } else {
+            image_buffer.extend_from_slice(&data);
+        }
+    }
+    Ok(())
 }
 
 /// Parses an image buffer into a `DynamicImage`.
