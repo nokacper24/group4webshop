ddaa305de984e2212a5576827358a76f6e0f090e
Oransj <98455426+Oransj@users.noreply.github.com>
2023-03-08 11:51:23 +0100

    Add functionality to AccordionTable

--- webshop_frontend/src/Components/product-managing/Accordion/AccordionTable.tsx
+++ webshop_frontend/src/Components/product-managing/Accordion/AccordionTable.tsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import { AccordionHeader } from "./AccordionHeader";
+import { AccordionRowProps } from "./AccordionRow";
 
 export default function AccordionTable() {
-    return (
-        <div className="accordion-table">
-            <AccordionHeader></AccordionHeader>
-            <AccordionHeader></AccordionHeader>
-        </div>
-    )
+    const [rowList, setRows] = useState<AccordionRowProps[]>();
+
+  const editAccordion = (id: number) => {
+    console.log("edit");
+  };
+
+  const deleteAccordion = (id: number) => {
+    console.log("delete");
+  };
+
+  return (
+    <div className="accordion-table">
+      <AccordionHeader rows={rows}></AccordionHeader>
+    </div>
+  );
 }
