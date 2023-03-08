4c62950b8b1aeff2d3e5bdd01058e2b980ba5a5d
Kacper Lukasz Nowicki <kacperln@ntnu.no>
2023-03-08 08:51:38 +0100

    refactor: :fire: remove deprecated multipart etractor
    replaced by more generic and reusable function

--- webshop_server/src/routes/private/products_protected/descriptions_protected/description_utils.rs
+++ webshop_server/src/routes/private/products_protected/descriptions_protected/description_utils.rs
@@ -1,80 +1,7 @@
 use actix_multipart::Multipart;
 use futures::StreamExt;
-use image::{io::Reader as ImageReader, DynamicImage, ImageError, ImageFormat, ImageResult};
+use image::{io::Reader as ImageReader, DynamicImage, ImageError, ImageFormat};
 use std::{collections::HashMap, io::Cursor};
-
-/// Extracts the image component from a multipart request.
-///
-/// # Arguments
-///
-/// * `payload` - The multipart payload to extract the image component from.
-///
-/// # Returns
-///
-/// A tuple containing the alt text, the image buffer and the file name.
-/// * `alt_text` - The alt text of the image.
-/// * `image_buffer` - Vector containing the image data as bytes.
-/// * `file_name` - The file name of the image.
-///
-/// # Errors
-///
-/// Returns an `ImageExtractorError` containing the error that occurred.
-#[deprecated]
-pub async fn extract_image_component_from_multipart(
-    mut payload: Multipart,
-) -> Result<(String, Vec<u8>, String), ImageExtractorError> {
-    let mut alt_text = String::new();
-    let mut file_name = String::new();
-    let mut image_buffer = Vec::new();
-
-    while let Some(mut item) = payload.next().await {
-        let field = match item {
-            Ok(ref mut field) => field,
-            Err(e) => return Err(ImageExtractorError::MultipartError(e)),
-        };
-        let name = match field.content_disposition().get_name() {
-            Some(name) => name,
-            None => return Err(ImageExtractorError::MissingField("name".to_string())),
-        };
-        if name == "alt_text" {
-            while let Some(chunk) = field.next().await {
-                let data = match chunk {
-                    Ok(data) => data,
-                    Err(e) => return Err(ImageExtractorError::MultipartError(e)),
-                };
-                let string = match std::str::from_utf8(&data) {
-                    Ok(s) => s,
-                    Err(e) => return Err(ImageExtractorError::Utf8Error(e)),
-                };
-                alt_text.push_str(string);
-            }
-        } else if name == "image" {
-            file_name = match field.content_disposition().get_filename() {
-                Some(name) => name.to_string(),
-                None => return Err(ImageExtractorError::MissingField("filename".to_string())),
-            };
-
-            while let Some(chunk) = field.next().await {
-                let data = match chunk {
-                    Ok(data) => data,
-                    Err(e) => return Err(ImageExtractorError::MultipartError(e)),
-                };
-                if image_buffer.len() + data.len() > super::MAX_IMAGE_SIZE {
-                    return Err(ImageExtractorError::FileTooLarge);
-                } else {
-                    image_buffer.extend_from_slice(&data);
-                }
-            }
-        } else {
-            return Err(ImageExtractorError::UnexpectedField(name.to_string()));
-        }
-    }
-    if alt_text.trim().is_empty() || file_name.trim().is_empty() || image_buffer.is_empty() {
-        return Err(ImageExtractorError::MissingData);
-    }
-
-    Ok((alt_text, image_buffer, file_name))
-}
 
 pub enum ImageExtractorError {
     Utf8Error(std::str::Utf8Error),
@@ -112,7 +39,6 @@ pub async fn extract_image_and_texts_from_multipart(
     let mut fields_found: HashMap<String, String> = HashMap::new();
 
     while let Some(mut item) = payload.next().await {
-        log::warn!("item: {:?}", item);
         let field = match item {
             Ok(ref mut field) => field,
             Err(e) => return Err(ImageExtractorError::MultipartError(e)),
