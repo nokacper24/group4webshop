1ff819f88c0cba23767dc385252af69b8255a51a
Kacper Lukasz Nowicki <kacperln@ntnu.no>
2023-03-13 08:35:45 +0100

    feat: :lock: add backend_user to the database
    this user has minimal permisions, and should be used by the backend application(webserver). Note that password to this user is stored in plain text and this is only ok in dev enviroment, in production the password must be secure and not shared
    
    closes #73

--- database/sql_scripts/init.sql
+++ database/sql_scripts/init.sql
@@ -164,4 +164,6 @@ CREATE TRIGGER delete_description_component_trigger
 AFTER DELETE ON description_component
 FOR EACH ROW EXECUTE FUNCTION delete_description_component();
 
+CREATE USER backend_user WITH PASSWORD 'password';
+GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public TO backend_user;
 COMMIT;
