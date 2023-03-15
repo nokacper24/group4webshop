522ceffd1ee56ed7d663262abd6145ffd4c77c2d
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-03-15 11:29:41 +0100

    style: :lipstick: style full width component separetly

--- webshop_frontend/src/Components/product-page/DescriptionsContainer.tsx
+++ webshop_frontend/src/Components/product-page/DescriptionsContainer.tsx
@@ -3,115 +3,110 @@ import { NewDescription } from "./ProductPage";
 let baseUrl = import.meta.env.VITE_URL + ":" + import.meta.env.VITE_PORT + "/";
 // check if we are in production mode
 if (import.meta.env.PROD) {
-    baseUrl = "/";
+  baseUrl = "/";
 }
 
 export default function DescriptionsContianer(descriptions: NewDescription[]) {
-    let prev_item: undefined | NewDescription = undefined as undefined | NewDescription;
-    return (
-        <>
-            {descriptions.map((item) => {
-                if (item.full_width && prev_item !== undefined) {
-                    let temp = prev_item;
-                    prev_item = undefined;
-                    return (
-                        <>
-                            <DescriptionRow
-                                descriptions={[temp]}
-                                key={temp.component_id}
-                            />
-                            <DescriptionRow
-                                descriptions={[item]}
-                                key={item.component_id}
-                            />
-                        </>
-                    );
-                } else if (item.full_width && prev_item === undefined) {
-                    return (
-                        <DescriptionRow
-                            descriptions={[item]}
-                            key={item.component_id}
-                        />);
-                } else if (!item.full_width && prev_item !== undefined) {
-                    let temp = prev_item;
-                    prev_item = undefined;
-                    return (
-
-                        <DescriptionRow
-                            descriptions={[temp, item]}
-                            key={temp.component_id}
-                        />
-                    );
-                } else if (!item.full_width && prev_item === undefined) {
-                    prev_item = item;
-                }
-            }
-            )}
-            {prev_item !== undefined && (
-                <DescriptionRow
-                    descriptions={[prev_item]}
-                    key={prev_item.component_id}
-                />
-            )
-            }
-        </>
-    );
+  let prev_item: undefined | NewDescription = undefined as
+    | undefined
+    | NewDescription;
+  return (
+    <>
+      {descriptions.map((item) => {
+        if (item.full_width && prev_item !== undefined) {
+          let temp = prev_item;
+          prev_item = undefined;
+          return (
+            <>
+              <DescriptionRow descriptions={[temp]} key={temp.component_id} />
+              <DescriptionRow descriptions={[item]} key={item.component_id} />
+            </>
+          );
+        } else if (item.full_width && prev_item === undefined) {
+          return (
+            <DescriptionRow descriptions={[item]} key={item.component_id} />
+          );
+        } else if (!item.full_width && prev_item !== undefined) {
+          let temp = prev_item;
+          prev_item = undefined;
+          return (
+            <DescriptionRow
+              descriptions={[temp, item]}
+              key={temp.component_id}
+            />
+          );
+        } else if (!item.full_width && prev_item === undefined) {
+          prev_item = item;
+        }
+      })}
+      {prev_item !== undefined && (
+        <DescriptionRow
+          descriptions={[prev_item]}
+          key={prev_item.component_id}
+        />
+      )}
+    </>
+  );
 }
 
-
-
 interface DescriptionRowProps {
-    descriptions: NewDescription[]
+  descriptions: NewDescription[];
 }
 
 function DescriptionRow(props: DescriptionRowProps) {
-    return (
-        <section className="description-row">
-            {props.descriptions.map((item) => {
-                if (item.image !== null) {
-                    return (
-                        <ImageDescription
-                            key={item.component_id}
-                            description={item}
-                        />
-                    );
-                } else if (item.text !== null) {
-                    return (
-                        <TextDescription
-                            key={item.component_id}
-                            description={item}
-                        />
-                    );
-                }
-            })}
-        </section>
-    )
+  return (
+    <section className="description-row">
+      {props.descriptions.map((item) => {
+        if (item.image !== null) {
+          return (
+            <ImageDescription key={item.component_id} description={item} />
+          );
+        } else if (item.text !== null) {
+          return <TextDescription key={item.component_id} description={item} />;
+        }
+      })}
+    </section>
+  );
 }
 
 interface ImageDescriptionProps {
-    description: NewDescription
+  description: NewDescription;
 }
 
 function ImageDescription(props: ImageDescriptionProps) {
-    return (
-        <div className="description-component image-description">
-        <img
-            src={baseUrl + props.description.image?.image_path}
-            alt={props.description.image?.alt_text}
-        />
-        </div>
-    )
+  let classes: string;
+  if (props.description.full_width) {
+    classes = "description-component image-description full-width-desc";
+  } else {
+    classes = "description-component image-description";
+  }
+
+  return (
+    <div className={classes}>
+      <img
+        src={baseUrl + props.description.image?.image_path}
+        alt={props.description.image?.alt_text}
+      />
+    </div>
+  );
 }
 
 interface TextDescriptionProps {
-    description: NewDescription
+  description: NewDescription;
 }
 
 function TextDescription(props: TextDescriptionProps) {
-    return (
-        <div className="description-component text-description">
-            <h2>{props.description.text?.text_title}</h2>
-            <p>{props.description.text?.paragraph}</p>
-        </div>
-    )
+  let classes: string;
+  if (props.description.full_width) {
+    classes = "description-component text-description full-width-desc";
+  } else {
+    classes = "description-component text-description";
+  }
+
+  return (
+    <div className={classes}>
+      <h2>{props.description.text?.text_title}</h2>
+      <p>{props.description.text?.paragraph}</p>
+    </div>
+  );
 }
