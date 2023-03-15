f1c9f5223d260677756f286879fe8049c56bdd92
Oransj <98455426+Oransj@users.noreply.github.com>
2023-03-15 13:45:15 +0100

    feat: :lipstick: add mapping

--- webshop_frontend/src/Components/product-managing/Accordion/AccordionBody.tsx
+++ webshop_frontend/src/Components/product-managing/Accordion/AccordionBody.tsx
@@ -1,2 +1,14 @@
+import { useState } from "react";
+import { AccordionRowProps } from "./AccordionRow";
+
 export function AccordionBody() {
+  const [rowList, setRows] = useState<AccordionRowProps[]>([]);
+
+  return (
+    <>
+      {rowList.map((row) => {
+        return <AccordionRowProps></AccordionRowProps>;
+      })}
+    </>
+  );
 }
