e9a4a3afa026f712548bb817bce62b0ed0a1563f
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-03-20 11:16:29 +0100

    fix: :ambulance: add https for frontend
    now it can call the api
    
    you need the certificates in frontend folder, you cancopy them over from backend folder

--- webshop_frontend/vite.config.ts
+++ webshop_frontend/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSSL from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    basicSSL()
+  ],
+  server: {
+    proxy: {
+      '/': {
+        target: 'https://localhost:8083',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
