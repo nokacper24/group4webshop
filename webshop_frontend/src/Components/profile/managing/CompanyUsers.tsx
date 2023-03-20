749b88c9c2c73ad8a6551119f3c608da30fc96bc
z-rayc <zilerayser@gmail.com>
2023-03-20 08:52:57 +0100

    feat: :sparkles: create form and functions for saving

--- webshop_frontend/src/Components/profile/managing/CompanyUsers.tsx
+++ webshop_frontend/src/Components/profile/managing/CompanyUsers.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { SelectTableRowProps } from "../managing/ManageLicenseAccess";
 import SelectTable from "./SelectTable";
+import { User } from "../../../Interfaces";
 
-type UserProps = {
-  userId: string;
-  email: string;
-};
-
+/**
+ * A page for managing company users.
+ * Users can be removed from the company, which revokes all their license accesses.
+ *
+ * @returns A manage company users page.
+ */
 export default function CompanyUsers() {
   let baseUrl = import.meta.env.VITE_URL + ":" + import.meta.env.VITE_PORT;
   // Check if we are in production mode
@@ -20,6 +22,9 @@ export default function CompanyUsers() {
   const [users, setUsers] = useState<SelectTableRowProps[]>([]);
   const [newRemovedUsers] = useState<Set<string>>(new Set());
 
+  const singleEmail = useRef<HTMLInputElement>(null);
+  const csvEmail = useRef<HTMLInputElement>(null);
+
   const editUser = () => {
     console.log("Editing user...");
   };
@@ -29,12 +34,12 @@ export default function CompanyUsers() {
    *
    * @param user The user to remove.
    */
-  const updateNewRemovedUsers = (user: any) => {
-    newRemovedUsers.add(user.id);
+  const updateNewRemovedUsers = (userId: string) => {
+    newRemovedUsers.add(userId);
   };
 
   /**
-   *  Remove selected users from list of users.
+   * Remove selected users from list of users.
    *
    * @param indices The indices of the users in the list.
    */
@@ -49,7 +54,7 @@ export default function CompanyUsers() {
         ...usersList.rows.slice(index + 1),
       ];
 
-      updateNewRemovedUsers(user);
+      updateNewRemovedUsers(user.id);
     }
 
     setUsers(usersList.rows);
@@ -73,20 +78,20 @@ export default function CompanyUsers() {
     const response = await fetch(`${baseUrl}/api/companies/${companyId}/users`);
     const data = await response.json();
 
-    const users: UserProps[] = [];
+    const users: User[] = [];
 
-    data.map((user: any) => {
+    data.map((user: User) => {
       if (user.role != "Admin" && user.role != "CompanyItHead") {
-        users.push({
-          userId: user.user_id,
-          email: user.email,
-        });
+        users.push(user);
       }
     });
     return users;
   };
 
-  const sendDeleteRequest = async () => {
+  /**
+   * Send a DELETE request to remove users.
+   */
+  const sendDeleteUsersRequest = async () => {
     fetch(`${baseUrl}/api/users`, {
       method: "DELETE",
       headers: {
@@ -112,6 +117,9 @@ export default function CompanyUsers() {
       .catch(() => alert("Failed to save users"));
   };
 
+  /**
+   * Save the changes made to users.
+   */
   const handleSave = () => {
     if (newRemovedUsers.size > 0) {
       if (
@@ -119,17 +127,107 @@ export default function CompanyUsers() {
           "Are you sure you want to remove the users? This action cannot be reversed."
         )
       ) {
-        sendDeleteRequest();
+        sendDeleteUsersRequest();
       }
+    }
+  };
+
+  /**
+   * Set the single email input to be empty.
+   */
+  const resetSingleEmailInput = () => {
+    if (singleEmail.current) {
+      singleEmail.current.value = "";
+    }
+  };
+
+  /**
+   * Set the CSV email input to be empty.
+   */
+  const resetCsvEmailInput = () => {
+    if (csvEmail.current) {
+      csvEmail.current.value = "";
+    }
+  };
+
+  /**
+   * Send a POST request for registering user.
+   *
+   * @param email The e-mail for the user to be created.
+   */
+  const sendPostRegisterUserRequest = async (email: string) => {
+    const formData = new FormData();
+    formData.append("email", email);
+
+    fetch(`${baseUrl}/api/register_user`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: formData,
+    })
+      .then((response) => {
+        const status = response.status;
+        if (status == 200) {
+          resetSingleEmailInput();
+        } else {
+          alert("Something went wrong when adding user");
+        }
+      })
+      .catch(() => alert("Failed to add user"));
+  };
+
+  /**
+   * Send a POST request for registering users.
+   *
+   * @param filePath The file path for the CSV file of users to be created.
+   */
+  const sendPostRegisterUsersRequest = async (filePath: string) => {
+    const formData = new FormData();
+    formData.append("csvFile", filePath);
+
+    fetch(`${baseUrl}/api/register_users`, {
+      method: "POST",
+      headers: {
+        Accept: "multipart/form-data",
+        "Content-Type": "multipart/form-data",
+      },
+      body: formData,
+    })
+      .then((response) => {
+        const status = response.status;
+        if (status == 200) {
+          resetCsvEmailInput();
+        } else {
+          alert("Something went wrong when adding users");
+        }
+      })
+      .catch(() => alert("Failed to add users"));
+  };
+
+  const handleSubmitSingleEmail = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (singleEmail.current && singleEmail.current.value != "") {
+      sendPostRegisterUserRequest(singleEmail.current.value);
+    }
+  };
+
+  const handleSubmitCsvEmail = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (csvEmail.current && csvEmail.current?.value != "") {
+      sendPostRegisterUsersRequest(csvEmail.current.value);
     }
   };
 
   useEffect(() => {
     fetchCompanyUsers().then((users) => {
       setUsers(
-        users.map((user: UserProps) => {
+        users.map((user: User) => {
           return {
-            id: user.userId,
+            id: user.user_id,
             columns: [{ text: user.email }],
           };
         })
@@ -146,6 +244,11 @@ export default function CompanyUsers() {
           role 'Company IT head' or 'Company IT'. In order to remove 'Company
           IT' users, you first have to remove the role from them.
         </p>
+        <p>
+          Once saved, these changes cannot be undone. The removed user accounts
+          will be deleted. They can create a new account with the same e-mail
+          address, but will have to go through all the initial steps again.
+        </p>
         <SelectTable
           header={usersList.header}
           rows={usersList.rows}
@@ -158,6 +261,50 @@ export default function CompanyUsers() {
       </section>
       <section className="container left-aligned">
         <h1>Add users</h1>
+        <p>
+          Add users by writing their e-mail in the field below. Alternatively,
+          you can upload a comma separated file (CSV) with multiple e-mails.
+          They will then get an invitation to create a user with that e-mail
+          address.
+        </p>
+        <form onSubmit={handleSubmitSingleEmail}>
+          <label style={{ display: "inline-block" }}>
+            E-mail
+            <input
+              ref={singleEmail}
+              style={{ marginInline: "1em", padding: "0.5em" }}
+              type="email"
+              name="email"
+              placeholder="john@doe.com"
+            ></input>
+          </label>
+
+          <button
+            style={{ display: "inline-block" }}
+            className="default-button small-button"
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
+        <form className="m-t-1" onSubmit={handleSubmitCsvEmail}>
+          <label style={{ display: "inline-block" }}>
+            Choose a file
+            <input
+              ref={csvEmail}
+              style={{ marginInline: "1em", padding: "0.5em" }}
+              type="file"
+              accept=".csv"
+            ></input>
+          </label>
+          <button
+            style={{ display: "inline-block" }}
+            className="default-button small-button"
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
       </section>
     </>
   );
