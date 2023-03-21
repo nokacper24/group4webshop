574cc6b8325a637108ce529cef071f9866408b8c
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-03-21 17:16:34 +0100

    refactor: :recycle: simplify validate_user

--- webshop_server/src/middlewares/auth.rs
+++ webshop_server/src/middlewares/auth.rs
@@ -1,13 +1,11 @@
 use crate::data_access::{
     self,
-    auth::{self},
+    auth,
     user::{Role, User},
 };
 
 use actix_web::{
-    dev::ServiceRequest,
-    web::{self, ReqData},
-    Error, HttpMessage, HttpResponse, Result, HttpRequest,
+    web::ReqData, HttpRequest, Result,
 };
 
 use serde::{Deserialize, Serialize};
@@ -27,43 +25,41 @@ pub async fn get_cookie(cookie: &str, pool: &Pool<Postgres>) -> Result<auth::Coo
 }
 
 /// Returns the user associated with the the "Secret" cookie in the request.
-pub async fn validate_user(req: HttpRequest, pool: &Pool<Postgres>) -> Result<User, AuthError> {
+pub async fn validate_user(req: HttpRequest, pool: &Pool<Postgres>) -> Result<User, AuthenticatorError> {
     match req.cookie(COOKIE_KEY_SECRET) {
-        None => {
-            return Err(AuthError::NoSecret);
-        }
         Some(cookie) => {
             let cookie = cookie.value();
-            let valid = check_auth(cookie, &pool).await;
-
-            match valid {
-                true => {
-                    let cookie = get_cookie(cookie, &pool).await;
-                    return match cookie {
-                        Ok(cookie) => {
-                            let user =
-                                data_access::user::get_user_by_id(&pool, cookie.user_id).await;
-                            return match user {
-                                Ok(user) => Ok(user),
-                                Err(e) => Err(AuthError::SqlxError(e)),
-                            };
-                        }
-                        Err(e) => Err(AuthError::SqlxError(e)),
-                    };
-                }
-                false => {
-                    return Err(AuthError::InvalidCookie);
-                }
+            let valid = match auth::is_valid_cookie(pool, cookie).await{
+                Ok(valid) => valid,
+                Err(e) => return Err(AuthenticatorError::SqlxError(e)),
+            };
+            if valid {
+                let cookie = get_cookie(cookie, &pool).await;
+                return match cookie {
+                    Ok(cookie) => {
+                        let user = data_access::user::get_user_by_id(&pool, cookie.user_id).await;
+                        return match user {
+                            Ok(user) => Ok(user),
+                            Err(e) => Err(AuthenticatorError::SqlxError(e)),
+                        };
+                    }
+                    Err(e) => Err(AuthenticatorError::SqlxError(e)),
+                };
+            } else {
+                return Err(AuthenticatorError::Unauthorized);
             }
+        }
+        None => {
+            return Err(AuthenticatorError::Unauthorized);
         }
     }
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
+#[deprecated]
 pub struct Token {
     pub token: String,
 }
-
 
 /// Extracts the token from the request and returns the role
 ///  shorthand for getting role from a logged in user
@@ -79,7 +75,7 @@ pub struct Token {
 ///    Err(e) => Err(e),
 ///     }
 /// }
-#[deprecated="Use validator instead"]
+#[deprecated = "Use validate_user instead"]
 pub async fn check_role(
     req_token: Option<ReqData<Token>>,
     pool: &Pool<Postgres>,
@@ -106,9 +102,15 @@ pub async fn check_role(
 /// Error type for auth
 /// BadToken: The token was not found or is invalid - do 401 Unauthorized
 /// SqlxError: An error occured while querying the database - probably 500 Internal Server Error
+#[deprecated]
 pub enum AuthError {
     SqlxError(sqlx::Error),
     NoSecret,
     BadToken,
     InvalidCookie,
+}
+
+pub enum AuthenticatorError {
+    SqlxError(sqlx::Error),
+    Unauthorized,
 }
