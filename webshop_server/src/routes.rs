b8a81316406aed51334b80defb8d7fb507579d8e
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-03-21 10:46:20 +0100

    fix: :goal_net: propper json 404 for api routes
    also moved not_found function to routes module

--- webshop_server/src/routes.rs
+++ webshop_server/src/routes.rs
@@ -1,4 +1,28 @@
+use actix_web::{HttpResponse, Responder};
+use serde_json::json;
+
 pub mod public;
 pub mod private;
 pub mod openapi_doc;
 pub mod serving_images;
+
+pub async fn not_found() -> impl Responder {
+    HttpResponse::NotFound().body(
+        r#"<html>
+    <head>
+        <title>404 Not Found</title>
+    </head>
+    <body>
+        <h1>404 Not Found</h1>
+        <p>The resource could not be found.</p>
+    </body>
+</html>"#,
+    )
+}
+
+pub async fn api_not_found() -> impl Responder {
+    HttpResponse::NotFound().json(json!({
+        "error": "Not Found",
+        "message": "The resource could not be found."
+    }))
+}
