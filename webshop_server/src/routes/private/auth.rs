485cc7210d8a5759bc6a1d7c05f3db6570f5d223
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-03-21 17:46:29 +0100

    fix: :adhesive_bandage: use new auth api

--- webshop_server/src/routes/private/auth.rs
+++ webshop_server/src/routes/private/auth.rs
@@ -1,16 +1,11 @@
-use actix_web::{
-    cookie::time::Duration,
-    get, post,
-    web::{self, ReqData},
-    HttpRequest, HttpResponse, Responder,
-};
+use actix_web::{cookie::time::Duration, get, post, web, HttpRequest, HttpResponse, Responder};
 use log::error;
 
 use sqlx::{Pool, Postgres};
 
 use crate::{
     data_access::auth::delete_cookie,
-    middlewares::auth::{validate_user, AuthError, Token, COOKIE_KEY_SECRET},
+    middlewares::auth::{self, AuthError, COOKIE_KEY_SECRET},
 };
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
@@ -19,15 +14,21 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
 }
 
 #[post("/logout")]
-async fn logout(pool: web::Data<Pool<Postgres>>, req: Option<ReqData<Token>>) -> impl Responder {
-    let token = match req {
-        Some(token) => token.into_inner(),
-        None => return HttpResponse::Unauthorized().finish(),
+async fn logout(pool: web::Data<Pool<Postgres>>, req: HttpRequest) -> impl Responder {
+    let cookie = match auth::extract_valid_cookie(req, &pool).await {
+        Ok(token) => token,
+        Err(e) => {
+            return match e {
+                AuthError::Unauthorized => HttpResponse::Unauthorized().finish(),
+                AuthError::SqlxError(e) => {
+                    error!("{}", e);
+                    HttpResponse::InternalServerError().finish()
+                }
+            }
+        }
     };
-    // delete cookie from db
-    let token = token.token;
 
-    let cookie = delete_cookie(&pool, &token).await;
+    let cookie = delete_cookie(&pool, &cookie).await;
     match cookie {
         Ok(_) => HttpResponse::Ok()
             .cookie(
@@ -37,13 +38,16 @@ async fn logout(pool: web::Data<Pool<Postgres>>, req: Option<ReqData<Token>>) ->
                     .finish(),
             )
             .finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
+        Err(e) => {
+            error!("{}", e);
+            HttpResponse::InternalServerError().finish()
+        }
     }
 }
 
 #[get("/logged_in")]
 async fn logged_in(req: HttpRequest, pool: web::Data<Pool<Postgres>>) -> impl Responder {
-    let valid = validate_user(req, &pool).await;
+    let valid = auth::validate_user(req, &pool).await;
     match valid {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => match e {
