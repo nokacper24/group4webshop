c13bd92899990d267749a6f56a1b6e3d88b22ca8
Oransj <98455426+Oransj@users.noreply.github.com>
2023-03-23 19:37:29 +0100

    fix: :rotating_light: fix unique key not being assigned

--- webshop_frontend/src/Components/product-managing/Accordion/AccordionTable.tsx
+++ webshop_frontend/src/Components/product-managing/Accordion/AccordionTable.tsx
@@ -78,6 +78,7 @@ export default function AccordionTable() {
       {sectionList.map((section) => {
         return (
           <AccordionSection
+            key={section.sectionID}
             header={section.header}
             sectionID={section.sectionID}
             registerContentChange={section.registerContentChange}
