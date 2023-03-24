71e9a8e49f12eb6d4d1cd101772d9ee81373f1f0
Oransj <chrion@stud.ntnu.no>
2023-03-24 14:27:02 +0100

    style: :art: format using prettier

--- webshop_frontend/src/Components/product-managing/Accordion/AccordionHeader.tsx
+++ webshop_frontend/src/Components/product-managing/Accordion/AccordionHeader.tsx
@@ -45,8 +45,10 @@ export function AccordionHeader(props: AccordionHeaderProps) {
             <path d="M240 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H176V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H240V80z" />
           </svg>
         </button>
-        <button className="remove-button overlay-button accordion-button-icon"
-        onClick={() => props.deleteSelf()}>
+        <button
+          className="remove-button overlay-button accordion-button-icon"
+          onClick={() => props.deleteSelf()}
+        >
           <svg
             className="header-icon"
             xmlns="http://www.w3.org/2000/svg"
@@ -57,8 +59,6 @@ export function AccordionHeader(props: AccordionHeaderProps) {
           </svg>
         </button>
       </div>
-
-
     </>
   );
 }
