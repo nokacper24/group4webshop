5557c20f10bdd07fb759a3b3cc7c2de99a3b2823
Oransj <chrion@stud.ntnu.no>
2023-03-24 14:25:42 +0100

    Style: :art: Format using prettier

--- webshop_frontend/src/Components/product-managing/Accordion/AccordionSection.tsx
+++ webshop_frontend/src/Components/product-managing/Accordion/AccordionSection.tsx
@@ -23,15 +23,14 @@ export type AccordionSectionProps = {
 
 /**
  * The main component for managing a header and its body.
- * 
+ *
  * @param props the props of the component, must be of AccordionSectionProps type
  * @returns the React component for the Accordion section
  */
 export function AccordionSection(props: AccordionSectionProps) {
-
   /**
    * Deletes a row from the body of the section.
-   * 
+   *
    * @param id the ID of the row to be deleted
    */
   const deleteRow = (id: number) => {
@@ -42,7 +41,7 @@ export function AccordionSection(props: AccordionSectionProps) {
 
   /**
    * Initializes the process of changing the content of a row.
-   * 
+   *
    * @param id the ID of the row to be edited
    */
   const editRow = (id: number) => {
@@ -52,7 +51,7 @@ export function AccordionSection(props: AccordionSectionProps) {
 
   /**
    * Initializes the process of adding a row to the body of the section.
-   * 
+   *
    * @param title title of the row to be added
    */
   const addRow = (title: string) => {
@@ -80,7 +79,7 @@ export function AccordionSection(props: AccordionSectionProps) {
     console.log("Delete self");
     console.log(props);
     props.deleteSection(props.sectionID);
-  }
+  };
 
   const [content, setContent] = useState<AccordionHeaderProps>({
     title: props.header.title,
