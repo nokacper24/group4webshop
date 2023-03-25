ee00211fb108c19e6ae82ff8eb255f9334e2e773
Oransj <chrion@stud.ntnu.no>
2023-03-25 10:12:51 +0100

    feat: :sparkles: create RowEditPopupProps

--- webshop_frontend/src/Components/product-managing/Edit-popups/RowEditPopup.tsx
+++ webshop_frontend/src/Components/product-managing/Edit-popups/RowEditPopup.tsx
@@ -1,3 +1,9 @@
+export type RowEditPopupProps = {
+    image: boolean,
+    title: string | undefined,
+    content: string | undefined,
+};
+
 export default function RowEditPopup() {
     return (
         <div>
