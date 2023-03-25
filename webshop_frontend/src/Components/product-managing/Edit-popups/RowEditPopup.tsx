d67563826c40f73dc5ddd6aa3800c512e7e07825
Oransj <chrion@stud.ntnu.no>
2023-03-25 10:16:56 +0100

    docs: :memo: add documentation

--- webshop_frontend/src/Components/product-managing/Edit-popups/RowEditPopup.tsx
+++ webshop_frontend/src/Components/product-managing/Edit-popups/RowEditPopup.tsx
@@ -1,3 +1,9 @@
+/**
+ * Props for the RowEditPopup component.
+ * Image indicates if the content should be interpreted as an image or not.
+ * The title and content can be undefined if a new row is being created.
+ * If the content is an image, the title is the alt text.
+ */
 export type RowEditPopupProps = {
     image: boolean,
     title: string | undefined,
