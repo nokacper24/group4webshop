374fab3c2b44c6f843cae7ad4ee4c25fb58b4f23
Oransj <chrion@stud.ntnu.no>
2023-03-25 21:30:17 +0100

    feat: :lipstick: add conditional rendering if image or text

--- webshop_frontend/src/Components/product-managing/Edit-popups/RowEditPopup.tsx
+++ webshop_frontend/src/Components/product-managing/Edit-popups/RowEditPopup.tsx
@@ -5,15 +5,28 @@
  * If the content is an image, the title is the alt text.
  */
 export type RowEditPopupProps = {
-    image: boolean,
-    title: string | undefined,
-    content: string | undefined,
+  image: boolean;
+  title: string | undefined;
+  content: string | undefined;
 };
 
-export default function RowEditPopup() {
-    return (
-        <div>
-            
-        </div>
-    );
+export default function RowEditPopup(props: RowEditPopupProps) {
+  return (
+    <div className="popup-grey-zone">
+      <div className="popup-box">
+        <form className="popup-form">
+          {props.image ? (
+            <>
+            </>
+          ) : (
+            <>
+                rows={10}
+              />
+            </>
+          )}
+
+        </form>
+      </div>
+    </div>
+  );
 }
