aee91e2df7ba711a014744641dcf881a54e40d2f
Oransj <98455426+Oransj@users.noreply.github.com>
2023-04-12 12:52:40 +0200

    feat: :lipstick: add functionality to HeaderEditPopup

--- webshop_frontend/src/Components/product-managing/Edit-popups/HeaderEditPopup.tsx
+++ webshop_frontend/src/Components/product-managing/Edit-popups/HeaderEditPopup.tsx
@@ -1,4 +1,70 @@
+import { RefObject, useRef } from "react";
+
+/**
+ * Props for the HeaderEditPopup component.
+ * The title can be undefined if a new header is being created.
+ * The informationCallBack is the function that should be called with the information from the form.
+ */
+export type HeaderEditPopupProps = {
+  title: string | undefined;
+  informationCallBack: (title: string) => void;
+};
+
+let popupRef: RefObject<HTMLDivElement>;
+let titleRef: RefObject<HTMLInputElement>;
+let props: HeaderEditPopupProps = {
+  title: undefined,
+  informationCallBack: () => {},
+};
 export default function RowEditPopup() {
+  popupRef = useRef<HTMLDivElement>(null);
+  titleRef = useRef(null);
+
   return (
+    <div className="popup-grey-zone" ref={popupRef}>
+      <div className="popup-box">
+        <form className="popup-form">
+          <h2>Edit paragraph</h2>
+          <label htmlFor="title">Title:</label>
+          <input type="text" ref={titleRef} id="title" name="title" />
+
+          <div>
+            <button
+              className="hero-button popup-button"
+              type="button"
+              onClick={() => save()}
+            >
+              Add
+            </button>
+            <button
+              className="hero-button popup-button"
+              type="button"
+              onClick={() => hide()}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
+}
+
+export function showHeaderPopup(inProps: HeaderEditPopupProps) {
+  props = inProps;
+  if (titleRef.current) {
+    titleRef.current.value = props.title ? props.title : "";
+  }
+  popupRef.current?.classList.add("popup-visible");
+}
+
+function save() {
+  props.informationCallBack(
+    titleRef.current ? titleRef.current.value : "Undefined Header"
+  );
+  hide();
+}
+
+function hide() {
+  popupRef.current?.classList.remove("popup-visible");
 }
