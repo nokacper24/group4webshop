b800db03651d12f03b0683395754c5501a14b3c7
z-rayc <zilerayser@gmail.com>
2023-04-17 08:59:20 +0200

    Create ToggleTableRow.tsx

---
+++ webshop_frontend/src/Components/profile/toggle-table/ToggleTableRow.tsx
@@ -0,0 +1,26 @@
+import ToggleButton from "./ToggleButton";
+import { ToggleTableRowProps } from "./ToggleTable";
+
+type RowProps = {
+  rowIndex: number;
+  row: ToggleTableRowProps;
+};
+
+export default function ToggleTableRow(props: RowProps) {
+  const handleClick = () => {
+    console.log("Click");
+  };
+
+  const row = props.row.row;
+
+  return (
+    <>
+      <tr>
+        <td>{row.text}</td>
+        <td style={{ width: "6em" }}>
+          <ToggleButton handleClick={handleClick} checked={row.toggleOn} />
+        </td>
+      </tr>
+    </>
+  );
+}
