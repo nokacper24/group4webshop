06e1c5a1c2c23080f68321d13b2e14ee2a1e71c1
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-04-19 14:42:38 +0200

    fix:

--- database/initialize_db.sh
+++ database/initialize_db.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 docker compose down
 docker compose up -d
+echo Docker compose done, waiting for 5 seconds...
+echo If it wails, increase the sleep time.
 sleep 5
 docker compose exec db bash -c "psql -U postgres -d proflex < /sql_scripts/initialize.sql"
-docker compose exec db bash -c "psql -U postgres -d proflex < /sql_scripts/populate_dev.sql"
-
-docker compose exec db bash -c "psql -U postgres -d proflex -c 'CREATE USER backend_user WITH PASSWORD \'password\';'" # escape the single quotes, fix this TODO
-docker compose exec db bash -c "psql -U postgres -d proflex -c 'GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public TO backend_user;'"
-docker compose exec db bash -c "psql -U postgres -d proflex -c 'GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO backend_user;'"
-
-# CREATE USER backend_user WITH PASSWORD 'password'; 
-# GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public TO backend_user; 
-# GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO backend_user;"
+echo "CREATE USER backend_user WITH PASSWORD 'password';" | docker compose exec -T db psql -U postgres -d proflex
+echo "GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public TO backend_user;" | docker compose exec -T db psql -U postgres -d proflex
+echo "GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO backend_user;" | docker compose exec -T db psql -U postgres -d proflex
