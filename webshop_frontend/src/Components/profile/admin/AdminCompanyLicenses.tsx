141598697ede95265fa89d6fc4ade728c5a90f15
z-rayc <zilerayser@gmail.com>
2023-04-19 09:46:04 +0200

    refactor: :recycle: separate patch request "then" response

--- webshop_frontend/src/Components/profile/admin/AdminCompanyLicenses.tsx
+++ webshop_frontend/src/Components/profile/admin/AdminCompanyLicenses.tsx
@@ -73,7 +73,8 @@ export default function AdminCompanyLicenses() {
   };
 
   /**
-   *  Remove selected licenses from list of valid licenses.
+   * Remove selected licenses from list of valid licenses,
+   * and add to list of invalid licenses.
    *
    * @param indices The indices of the licenses in the list.
    */
@@ -89,6 +90,12 @@ export default function AdminCompanyLicenses() {
     );
   };
 
+  /**
+   * Remove selected licenses from list of invalid licenses,
+   * and add to list of valid licenses.
+   *
+   * @param indices The indices of the licenses in the list.
+   */
   const validateSelectedLicenses = (indices: number[]) => {
     moveItemsBetweenTables(
       indices,
@@ -138,13 +145,7 @@ export default function AdminCompanyLicenses() {
         }),
       })
         .then((response) => {
-          const status = response.status;
-          if (status == 200) {
-            // Refresh
-            navigate(0);
-          } else {
-            alert("Something went wrong when saving licenses");
-          }
+          handlePatchLicenseResponse(response);
         })
         .catch(() => alert("Failed to save license validation status"));
     }
@@ -171,15 +172,23 @@ export default function AdminCompanyLicenses() {
         }),
       })
         .then((response) => {
-          const status = response.status;
-          if (status == 200) {
-            // Refresh
-            navigate(0);
-          } else {
-            alert("Something went wrong when saving licenses");
-          }
+          handlePatchLicenseResponse(response);
         })
         .catch(() => alert("Failed to save license validation status"));
+    }
+  };
+
+  /**
+   * Handle the response from a PATCH request sent for patching (saving) licenses.
+   *
+   * @param response The response from the fetch request.
+   */
+  const handlePatchLicenseResponse = (response: Response) => {
+    if (response.status == 200) {
+      // Refresh
+      navigate(0);
+    } else {
+      alert("Something went wrong when saving licenses");
     }
   };
 
