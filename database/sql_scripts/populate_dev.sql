0d9d692a321480e9e2dfeaa52119cfd0b9142f86
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-04-21 13:21:48 +0200

    fix: :technologist: add missing /

--- database/sql_scripts/populate_dev.sql
+++ database/sql_scripts/populate_dev.sql
@@ -18,12 +18,12 @@ VALUES ('cookie1', '2026-12-31', 2),
        ('admin_cookie', '4000-06-30', 1);
 
 INSERT INTO product (product_id, display_name, price_per_user, short_description, main_image, available)
-VALUES ('time_management_software', 'Time Management System', 999.99, 'A comprehensive time management solution for enterprises.', 'time_management.jpg', true),
-       ('3d_modelling_software', '3D Modelling', 29.99, 'A fast and easy-to-learn software for 3D modelling, with extensive tools for perfecting details.', '3d_modelling.jpg', true),
-       ('tax_and_accounting_software', 'Tax and Accounting Software', 899.99, 'A software solution for tax and accounting needs of enterprises.', 'tax_accounting.jpg', true),
-       ('online_scheduling_software', 'Online Scheduling Software', 200, 'Our software is designed to help you effectively manage and streamline the scheduling process. It helps you automate the scheduling process, allowing you to save time and reduce errors.', 'online_scheduling.jpg', true),
-       ('bpa_solutions', 'BPA Solutions', 500, 'Automation of processes. It has an intuitive user interface, and automates workflows.', 'bpa.jpg', true),
-       ('statistics_software',	'Statistics software',	1000,	'Enterprise oriented statistics software for your compoany.',	'resources/images/statistics_software/Kacpernt_enterprise_managment_software_e231d881-8b93-43d9-a42c-2cad1cc5d3be.png',	true);
+VALUES ('time_management_software', 'Time Management System', 999.99, 'A comprehensive time management solution for enterprises.', '/time_management.jpg', true),
+       ('3d_modelling_software', '3D Modelling', 29.99, 'A fast and easy-to-learn software for 3D modelling, with extensive tools for perfecting details.', '/3d_modelling.jpg', true),
+       ('tax_and_accounting_software', 'Tax and Accounting Software', 899.99, 'A software solution for tax and accounting needs of enterprises.', '/tax_accounting.jpg', true),
+       ('online_scheduling_software', 'Online Scheduling Software', 200, 'Our software is designed to help you effectively manage and streamline the scheduling process. It helps you automate the scheduling process, allowing you to save time and reduce errors.', '/online_scheduling.jpg', true),
+       ('bpa_solutions', 'BPA Solutions', 500, 'Automation of processes. It has an intuitive user interface, and automates workflows.', '/bpa.jpg', true),
+       ('statistics_software',	'Statistics software',	1000,	'Enterprise oriented statistics software for your compoany.',	'/resources/images/statistics_software/Kacpernt_enterprise_managment_software_e231d881-8b93-43d9-a42c-2cad1cc5d3be.png',	true);
 
 INSERT INTO license (valid, start_date, end_date, amount, company_id, product_id)
 VALUES (false, '2022-01-01', '2022-12-31', 100, 2, 'time_management_software'),
