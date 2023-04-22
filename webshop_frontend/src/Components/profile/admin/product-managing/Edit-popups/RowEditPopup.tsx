77513bc031fb25a543e8db7649af08d923d5506e
Oransj <98455426+Oransj@users.noreply.github.com>
2023-04-22 13:40:05 +0200

    feat: :sparkles: add reset of values if parameters are empty

--- webshop_frontend/src/Components/profile/admin/product-managing/Edit-popups/RowEditPopup.tsx
+++ webshop_frontend/src/Components/profile/admin/product-managing/Edit-popups/RowEditPopup.tsx
@@ -10,11 +10,25 @@ export type RowEditPopupProps = {
   image: boolean;
   title: string | undefined;
   content: string | undefined;
+  informationCallBack: (image: boolean, title: string, content: string) => void;
 };
 
 let popupRef: RefObject<HTMLDivElement>;
-export default function RowEditPopup(props: RowEditPopupProps) {
-  popupRef = useRef<HTMLDivElement>(null);
+let imageRef: RefObject<HTMLInputElement>;
+let titleRef: RefObject<HTMLInputElement>;
+let altTextRef: RefObject<HTMLTextAreaElement>;
+let contentRef: RefObject<HTMLTextAreaElement>;
+let props: RowEditPopupProps = {
+  image: false,
+  title: undefined,
+  content: undefined,
+  informationCallBack: () => {},
+};
+export default function RowEditPopup() {
+  popupRef = useRef(null);
+  imageRef = useRef(null);
+  titleRef = useRef(null);
+  contentRef = useRef(null);
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -38,6 +52,8 @@ export default function RowEditPopup(props: RowEditPopupProps) {
                 id="image"
                 name="image"
                 accept="image/png, image/jpeg, image/webp"
+                ref={imageRef}
+                defaultValue={props.content ? props.content : ""}
               />
               <label htmlFor="alt-text">Alt-text:</label>
               <textarea
@@ -45,6 +61,8 @@ export default function RowEditPopup(props: RowEditPopupProps) {
                 id="alt-text"
                 cols={40}
                 rows={10}
+                ref={altTextRef}
+                defaultValue={props.title ? props.title : ""}
               ></textarea>
             </>
           ) : (
@@ -56,6 +74,7 @@ export default function RowEditPopup(props: RowEditPopupProps) {
                 id="title"
                 name="title"
                 defaultValue={props.title ? props.title : ""}
+                ref={titleRef}
               />
               <label htmlFor="paragraph">Paragraph:</label>
               <textarea
@@ -63,12 +82,17 @@ export default function RowEditPopup(props: RowEditPopupProps) {
                 defaultValue={props.content ? props.content : ""}
                 cols={40}
                 rows={10}
+                ref={contentRef}
               />
             </>
           )}
 
           <div>
-            <button className="hero-button popup-button" type="button">
+            <button
+              className="hero-button popup-button"
+              type="button"
+              onClick={() => save()}
+            >
               Save
             </button>
             <button
@@ -85,8 +109,27 @@ export default function RowEditPopup(props: RowEditPopupProps) {
   );
 }
 
-export function showPopup(func: () => void) {
+export function showPopup(inProps: RowEditPopupProps) {
+  props = inProps;
   popupRef.current?.classList.add("popup-visible");
+  imageRef.current
+    ? (imageRef.current.value = props.content ? props.content : "")
+    : undefined; //TODO: Implement "Something has gone wrong"
+  titleRef.current
+    ? (titleRef.current.value = props.title ? props.title : "")
+    : undefined; //TODO: Implement "Something has gone wrong"
+  contentRef.current
+    ? (contentRef.current.value = props.content ? props.content : "")
+    : undefined; //TODO: Implement "Something has gone wrong"
+}
+
+function save() {
+  props.informationCallBack(
+    props.image,
+    titleRef.current?.value ? titleRef.current.value : "",
+    contentRef.current?.value ? contentRef.current.value : ""
+  );
+  hidePopup();
 }
 
 function hidePopup() {
