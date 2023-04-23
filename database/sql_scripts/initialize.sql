796878a0bc99397a4eb1b69f5e260d75bf9b78e4
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-04-23 16:50:38 +0200

    fix: :card_file_box: make product_category rows cascade on product delete

--- database/sql_scripts/initialize.sql
+++ database/sql_scripts/initialize.sql
@@ -89,8 +89,8 @@ CREATE TABLE product_category (
     product_id TEXT NOT NULL,
     category_id INT NOT NULL,
     PRIMARY KEY (product_id, category_id),
-    FOREIGN KEY (product_id) REFERENCES product(product_id),
-    FOREIGN KEY (category_id) REFERENCES category(category_id)
+    FOREIGN KEY (product_id) REFERENCES product(product_id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE
 );
 
 CREATE TABLE testimonial (
