41d6b9917f84676269d25dec470830770132bbe0
z-rayc <zilerayser@gmail.com>
2023-04-24 16:01:49 +0200

    fix: :sparkles: add companies to private routes

--- webshop_server/src/routes/private.rs
+++ webshop_server/src/routes/private.rs
@@ -1,6 +1,7 @@
 use actix_web::{get, web, Responder};
 
 pub mod auth;
+pub mod companies_protected;
 pub mod licenses_protected;
 pub mod me;
 pub mod products_protected;
@@ -19,4 +20,5 @@ pub fn private(cfg: &mut web::ServiceConfig) {
     cfg.configure(me::configure);
     cfg.configure(users_protected::configure);
     cfg.configure(testimonials_protected::configure);
+    cfg.configure(companies_protected::configure);
 }
