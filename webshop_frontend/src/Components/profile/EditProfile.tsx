dfb7c2530f5eb8327bbea9531efec4ff85a5aa67
z-rayc <zilerayser@gmail.com>
2023-04-25 11:58:14 +0200

    perf: :fire: remove unused imports

--- webshop_frontend/src/Components/profile/EditProfile.tsx
+++ webshop_frontend/src/Components/profile/EditProfile.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { MeUser } from "../../Interfaces";
 import { resetPassword, patchPartialUser } from "../../ApiController";
