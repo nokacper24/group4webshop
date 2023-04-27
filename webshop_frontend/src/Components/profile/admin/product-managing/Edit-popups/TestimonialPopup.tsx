3d4b6712b64cd175670b98cd9ffb666538fde6b2
Oransj <98455426+Oransj@users.noreply.github.com>
2023-04-27 13:03:57 +0200

    feat: :sparkles: add testimonial specific HTML structure

--- webshop_frontend/src/Components/profile/admin/product-managing/Edit-popups/TestimonialPopup.tsx
+++ webshop_frontend/src/Components/profile/admin/product-managing/Edit-popups/TestimonialPopup.tsx
@@ -1,4 +1,78 @@
 export default function TestimonialPopup(
+  return (
+    <div className="popup-grey-zone" ref={popupRef}>
+      <div className="popup-box">
+        <form
+          className="popup-form"
+          onSubmit={(event) => {
+            handleSubmit(event);
+          }}
+        >
+          <h2>Edit testimonial</h2>
+          <label htmlFor="authorName">Author name:</label>
+          <input
+            type="text"
+            ref={authorNameRef}
+            id="authorName"
+            name="authorName"
+            defaultValue={props.testimonial ? props.testimonial.author : ""}
+          />
+          <label htmlFor="authorPic">Author picture:</label>
+          <input
+            type="file"
+            id="authorPic"
+            name="authorPic"
+            alt="Author picture"
+            accept="image/png, image/jpeg, image/webp"
+            onChange={() => {
+              props = {
+                ...props,
+                testimonial: {
+                  ...props.testimonial!,
+                  author_pic: authorPicRef.current?.value!,
+                },
+              };
+            }}
+            ref={authorPicRef}
+          />
+          <p>
+            Current image:{" "}
+            {props.testimonial?.author_pic
+              ? props.testimonial?.author_pic
+              : "none"}
+          </p>
+          <label htmlFor="testimonial-text">Testimonial:</label>
+          <textarea
+            name="testimonial-text"
+            id="testimonial-text"
+            cols={40}
+            rows={10}
+            ref={testimonialText}
+            defaultValue={props.testimonial ? props.testimonial.text : ""}
+          ></textarea>
+
+          <div>
+            <button
+              className="default-button small-button popup-button"
+              type="button"
+              onClick={() => save()}
+            >
+              Add
+            </button>
+            <button
+              className="default-button small-button popup-button"
+              type="button"
+              onClick={() => hidePopup()}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
 export function showTestimonialPopup(inProps: TestimonialPopupProps) {
   props = inProps;
   popupRef.current?.classList.add("popup-visible");
