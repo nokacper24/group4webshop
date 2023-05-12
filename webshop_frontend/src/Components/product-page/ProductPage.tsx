b48c67cd239887bcd64611ff027cbf28f82e7afe
z-rayc <zilerayser@gmail.com>
2023-05-12 23:17:42 +0200

    refactor: :recycle: move margin from container to banner

--- webshop_frontend/src/Components/product-page/ProductPage.tsx
+++ webshop_frontend/src/Components/product-page/ProductPage.tsx
@@ -84,7 +84,7 @@ export default function ProductPage() {
             </div>
           </section>
           {descriptions.length != 0 && (
-            <section style={{ marginTop: "2em" }} className="container">
+            <section className="container">
               {DescriptionsContainer(descriptions)}
             </section>
           )}
@@ -92,7 +92,7 @@ export default function ProductPage() {
             <section className="gallery-wrapper">
               <div className="container">
                 <h2 className="testimonial-title">Testimonials</h2>
-                <Gallery slides={testimonials} galleryName={"Testimonials"} />
+                <Gallery slides={testimonials} galleryName="Testimonials" />
               </div>
             </section>
           )}
